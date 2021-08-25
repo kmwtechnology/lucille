e0e6d8a6f63909fb29fdff3c5ed7ea97b79b1412
Rudi Seitz <rudi@kmwllc.com>
2021-08-25 14:56:00 -0400

    Adding doc.

--- src/main/java/com/kmwllc/lucille/message/PublisherMessageManager.java
+++ src/main/java/com/kmwllc/lucille/message/PublisherMessageManager.java
@@ -3,6 +3,12 @@ package com.kmwllc.lucille.message;
 import com.kmwllc.lucille.core.Document;
 import com.kmwllc.lucille.core.Event;
 
+/**
+ * API that a Publisher uses to exchange messages with other components.
+ *
+ * A Publisher needs to 1) submit Documents for processing and 2) receive Events
+ * relating to Documents it has published and their children.
+ */
 public interface PublisherMessageManager {
 
   void sendForProcessing(Document document) throws Exception;
