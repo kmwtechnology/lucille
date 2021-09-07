c41d195fcb8488ede373af7a9f9606af7284c919
Spencer Solomon <spencer.solomon@kmwllc.com>
2021-09-07 10:47:59 -0400

    Return after sending FAILED events.

--- src/main/java/com/kmwllc/lucille/core/Indexer.java
+++ src/main/java/com/kmwllc/lucille/core/Indexer.java
@@ -89,9 +89,11 @@ class Indexer implements Runnable {
           try {
             manager.sendEvent(new Event(d.getId(), d.getRunID(),
                 "FAILED" + e.getMessage(), Event.Type.INDEX, Event.Status.FAILURE));
+            return;
           } catch (Exception e2) {
             // TODO : Do something special if we get an error when sending Failure events
             e2.printStackTrace();
+            return;
           }
         }
       }
