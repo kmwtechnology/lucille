0af7e925ccdacb9f0c392df6b28e156f06c61ffa
kwatters <kwatters@kmwllc.com>
2021-09-15 10:39:41 -0400

    whitespace

--- src/test/java/com/kmwllc/lucille/connector/jdbc/DBTestHelper.java
+++ src/test/java/com/kmwllc/lucille/connector/jdbc/DBTestHelper.java
@@ -10,49 +10,50 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
+ * Database helper class to create an h2 database 
+ * and initialize it with a sql start script.
+ * 
  * Created by matt on 3/27/17.
  */
 public class DBTestHelper extends ExternalResource {
-    private final String driver;
-    private final String connection;
-    private final String user;
-    private final String password;
-    private final String startScriptPath;
-    private final String endScriptPath;
-    private Connection conn;
+  private final String driver;
+  private final String connection;
+  private final String user;
+  private final String password;
+  private final String startScriptPath;
+  private final String endScriptPath;
+  private Connection conn;
 
-    public DBTestHelper(String driver, String connection, String user, String password, String startScriptPath,
-                    String endScriptPath) {
-        this.driver = driver;
-        this.connection = connection;
-        this.user = user;
-        this.password = password;
-        this.startScriptPath = startScriptPath;
-        this.endScriptPath = endScriptPath;
+  public DBTestHelper(String driver, String connection, String user, String password, String startScriptPath,
+      String endScriptPath) {
+    this.driver = driver;
+    this.connection = connection;
+    this.user = user;
+    this.password = password;
+    this.startScriptPath = startScriptPath;
+    this.endScriptPath = endScriptPath;
+  }
+
+  @Override
+  protected void before() throws Throwable {
+    super.before();
+    Class.forName("org.h2.Driver");
+    conn = DriverManager.getConnection(connection);
+    InputStream in = DBTestHelper.class.getClassLoader().getResourceAsStream(startScriptPath);
+    RunScript.execute(conn, new InputStreamReader(in));
+    conn.commit();
+  }
+
+  @Override
+  protected void after() {
+    super.after();
+    try {
+      InputStream in = DBTestHelper.class.getClassLoader().getResourceAsStream(endScriptPath);
+      RunScript.execute(conn, new InputStreamReader(in));
+      conn.commit();
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-
-    @Override
-    protected void before() throws Throwable {
-        super.before();
-
-        Class.forName("org.h2.Driver");
-        conn = DriverManager.getConnection(connection);
-
-        InputStream in = DBTestHelper.class.getClassLoader().getResourceAsStream(startScriptPath);
-        RunScript.execute(conn, new InputStreamReader(in));
-        conn.commit();
-    }
-
-    @Override
-    protected void after() {
-        super.after();
-        try {
-            InputStream in = DBTestHelper.class.getClassLoader().getResourceAsStream(endScriptPath);
-            RunScript.execute(conn, new InputStreamReader(in));
-            conn.commit();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
