55aa7b86666bad43ddc751ec113683eb13621db0
Spencer Solomon <spencer.solomon@kmwllc.com>
2021-09-27 15:34:38 -0400

    Made updates to SolrConnector and associated tests.

--- src/main/java/com/kmwllc/lucille/connector/SolrConnector.java
+++ src/main/java/com/kmwllc/lucille/connector/SolrConnector.java
@@ -10,7 +10,6 @@ import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.request.GenericSolrRequest;
 import org.apache.solr.client.solrj.request.RequestWriter;
-import org.apache.solr.client.solrj.response.SimpleSolrResponse;
 import org.apache.solr.common.util.NamedList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,24 +18,25 @@ import java.lang.invoke.MethodHandles;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * Connector for issuing requests to Solr.
+ *
+ * Connector Parameters:
+ *
+ *   - preActions (List<String>, Optional) : A list of requests to be issued to Solr. These actions will be performed first.
+ *   - postActions (List<String>, Optional) : A list of requests to be issued to Solr. These actions will be performed second.
+ *   - solr.url (String) : The url of the Solr instance for this Connector to issue its requests to.
+ */
 public class SolrConnector extends AbstractConnector {
 
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private SolrClient client;
   private final GenericSolrRequest request;
+  private List<String> replacedActions;
 
   private List<String> postActions;
   private List<String> preActions;
-
-  // There's 2 approaches to this I can think of right now...
-  // 1. Have  lists of post/pre Strings, which are directly the requests to send to Solr. So if you wanted to commit,
-  // you would put "<commit/>" in the postActions lsit. We could have a syntax for replacing runId, but otherwise we can
-  // just iterate through the request Strings and issue them.
-  // 2. We have a map of action names to a list of parameters. The list of parameters would be different based on the
-  // action. We would support a subset of actions and each action would expect a certain number of parameters.
-  // For example, commit would have no params, but deleteByQuery would have to pass in the query string. This
-  // puts more of the Solr syntax under the hood but makes the config file more complex and specific in what we support.
 
   public SolrConnector(Config config) {
     super(config);
@@ -44,21 +44,24 @@ public class SolrConnector extends AbstractConnector {
     this.postActions = ConfigUtils.getOrDefault(config, "postActions", new ArrayList<>());
     this.client = new HttpSolrClient.Builder(config.getString("solr.url")).build();
     this.request = new GenericSolrRequest(SolrRequest.METHOD.POST, "/update", null);
+    this.replacedActions = new ArrayList<>();
   }
 
-  public SolrConnector(Config config, SolrClient client) {
+  public SolrConnector(Config config, SolrClient client, List<String> replacedActions) {
     this(config);
     this.client = client;
+    this.replacedActions = replacedActions;
   }
 
   @Override
-  public void preExecute(String runId) {
+  public void preExecute(String runId) throws ConnectorException {
+    replacedActions.clear();
     Map<String, String> replacement = new HashMap<>();
     replacement.put("runId", runId);
     StrSubstitutor sub = new StrSubstitutor(replacement, "{", "}");
-    preActions  = preActions.stream().map(sub::replace).collect(Collectors.toList());
+    replacedActions.addAll(preActions.stream().map(sub::replace).collect(Collectors.toList()));
 
-    executeActions(preActions);
+    executeActions(replacedActions);
   }
 
   @Override
@@ -66,16 +69,25 @@ public class SolrConnector extends AbstractConnector {
   }
 
   @Override
-  public void postExecute(String runId) {
+  public void postExecute(String runId) throws ConnectorException {
+    replacedActions.clear();
     Map<String, String> replacement = new HashMap<>();
     replacement.put("runId", runId);
     StrSubstitutor sub = new StrSubstitutor(replacement, "{", "}");
-    postActions = postActions.stream().map(sub::replace).collect(Collectors.toList());
+    replacedActions.addAll(postActions.stream().map(sub::replace).collect(Collectors.toList()));
 
-    executeActions(postActions);
+    executeActions(replacedActions);
   }
 
-  private void executeActions(List<String> actions) {
+  public List<String> getPreActions() {
+    return preActions;
+  }
+
+  public List<String> getPostActions() {
+    return postActions;
+  }
+
+  private void executeActions(List<String> actions) throws ConnectorException {
     for (String action : actions) {
       RequestWriter.ContentWriter contentWriter = new RequestWriter.StringPayloadContentWriter(action, "text/xml");
       request.setContentWriter(contentWriter);
@@ -84,7 +96,7 @@ public class SolrConnector extends AbstractConnector {
         NamedList<Object> resp = client.request(request);
         log.info(String.format("Action \"%s\" complete, response: %s", action, resp.asShallowMap().get("responseHeader")));
       } catch (Exception e) {
-        log.error("Failed to perform action: " + action, e);
+        throw new ConnectorException("Failed to perform action: " + action, e);
       }
     }
   }
--- src/test/java/com/kmwllc/lucille/connector/SolrConnectorTest.java
+++ src/test/java/com/kmwllc/lucille/connector/SolrConnectorTest.java
@@ -1,39 +1,59 @@
 package com.kmwllc.lucille.connector;
 
+import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.kmwllc.lucille.core.Connector;
 import com.kmwllc.lucille.core.ConnectorException;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.request.GenericSolrRequest;
-import org.apache.solr.client.solrj.request.RequestWriter;
 import org.apache.solr.common.util.NamedList;
 import org.junit.Test;
-import org.mockito.internal.stubbing.defaultanswers.GloballyConfiguredAnswer;
-import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class SolrConnectorTest {
 
   @Test
-  public void testPostActions() throws ConnectorException, SolrServerException, IOException {
+  public void testActions() throws ConnectorException, SolrServerException, IOException {
     Config config = ConfigFactory.load("SolrConnectorTest/config.conf");
     SolrClient mockClient = mock(SolrClient.class);
     when(mockClient.request(any(GenericSolrRequest.class))).thenReturn(new NamedList<>());
-    Connector connector = new SolrConnector(config, mockClient);
+    Connector connector = new SolrConnector(config, mockClient, new ArrayList<>());
 
     connector.preExecute("run");
     connector.postExecute("run");
 
+    // We expect the request method to be called 5 times, since we supplied 2 pre actions and 3 post actions.
     verify(mockClient, times(5)).request(any(GenericSolrRequest.class));
+  }
+
+  @Test(expected = ConnectorException.class)
+  public void testFailingActions() throws SolrServerException, IOException, ConnectorException {
+    Config config = ConfigFactory.load("SolrConnectorTest/config.conf");
+    SolrClient mockClient = mock(SolrClient.class);
+    when(mockClient.request(any(GenericSolrRequest.class))).thenThrow();
+    Connector connector = new SolrConnector(config, mockClient, new ArrayList<>());
+
+    connector.preExecute("run");
+    connector.postExecute("run");
+  }
+
+  @Test
+  public void testRunIdReplacement() throws SolrServerException, IOException, ConnectorException {
+    Config config = ConfigFactory.load("SolrConnectorTest/runId.conf");
+    SolrClient mockClient = mock(SolrClient.class);
+    when(mockClient.request(any(GenericSolrRequest.class))).thenReturn(new NamedList<>());
+    List<String> replacedActions = new ArrayList<>();
+    Connector connector = new SolrConnector(config, mockClient, replacedActions);
+
+    connector.preExecute("run1");
+    assertEquals("<delete><query>runId:run1</query></delete>", replacedActions.get(0));
   }
 }
