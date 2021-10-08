5778571eff34c095e78de828e332a5d49a8ba6e1
Spencer Solomon <spencer.solomon@kmwllc.com>
2021-10-08 09:47:28 -0400

    initial commit for TrimWhitespace Stage and associated test.

---
+++ src/main/java/com/kmwllc/lucille/stage/TrimWhitespace.java
@@ -0,0 +1,52 @@
+package com.kmwllc.lucille.stage;
+
+import com.kmwllc.lucille.core.Document;
+import com.kmwllc.lucille.core.Stage;
+import com.kmwllc.lucille.core.StageException;
+import com.kmwllc.lucille.core.UpdateMode;
+import com.typesafe.config.Config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This Stage removes leading and trailing whitespace from every value in the given list of fields.
+ *
+ * Config Parameters -
+ *
+ *   fields (List<String>) : The list of fields to trim whitespace from
+ */
+public class TrimWhitespace extends Stage {
+
+  private final List<String> fields;
+
+  public TrimWhitespace(Config config) {
+    super(config);
+    this.fields = config.getStringList("fields");
+  }
+
+  @Override
+  public void start() throws StageException {
+    if (fields.size() == 0)
+      throw new StageException("Must supply at least one field to trim whitespace from.");
+  }
+
+  @Override
+  public List<Document> processDocument(Document doc) throws StageException {
+    for (String field : fields) {
+      if (!doc.has(field))
+        continue;
+
+      List<String> values = new ArrayList<>();
+      for (String value : doc.getStringList(field)) {
+        values.add(value.trim());
+      }
+
+      doc.removeField(field);
+      doc.update(field, UpdateMode.DEFAULT, values.toArray(new String[0]));
+    }
+
+
+    return null;
+  }
+}
---
+++ src/test/java/com/kmwllc/lucille/stage/TrimWhitespaceTest.java
@@ -0,0 +1,40 @@
+package com.kmwllc.lucille.stage;
+
+import com.kmwllc.lucille.core.Document;
+import com.kmwllc.lucille.core.Stage;
+import com.kmwllc.lucille.core.StageException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TrimWhitespaceTest {
+
+  StageFactory factory = StageFactory.of(TrimWhitespace.class);
+
+  @Test
+  public void trimWhitespaceTest() throws StageException {
+    Stage stage = factory.get("TrimWhitespaceTest/config.conf");
+
+    Document doc1 = new Document("doc1");
+    doc1.setField("input1", "   trim this   ");
+    doc1.setField("input2", "   test");
+    doc1.addToField("input2", "test   ");
+    stage.processDocument(doc1);
+    assertEquals("trim this", doc1.getString("input1"));
+    assertEquals("test", doc1.getStringList("input2").get(0));
+    assertEquals("test", doc1.getStringList("input2").get(1));
+    assertFalse(doc1.has("input3"));
+
+    Document doc2 = new Document("doc2");
+    doc2.setField("input1", "nothing to trim");
+    doc2.addToField("input1", " trim this ");
+    doc2.addToField("input1", "     test");
+    doc2.addToField("input1", "test    ");
+    stage.processDocument(doc2);
+    assertEquals("nothing to trim", doc2.getStringList("input1").get(0));
+    assertEquals("trim this", doc2.getStringList("input1").get(1));
+    assertEquals("test", doc2.getStringList("input1").get(2));
+    assertEquals("test", doc2.getStringList("input1").get(3));
+
+  }
+
+}
