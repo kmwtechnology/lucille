80710da178d4aa6ff504329cf1b6ddb2fcd8f934
Spencer Solomon <spencer.solomon@kmwllc.com>
2021-10-15 11:08:01 -0400

    Changed RemoveDuplicateValues to use a fieldMapping rather than a list of fields to provide parity to the Attivio implementation.

--- src/main/java/com/kmwllc/lucille/stage/RemoveDuplicateValues.java
+++ src/main/java/com/kmwllc/lucille/stage/RemoveDuplicateValues.java
@@ -5,8 +5,12 @@ import com.kmwllc.lucille.core.Stage;
 import com.kmwllc.lucille.core.StageException;
 import com.kmwllc.lucille.core.UpdateMode;
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigValue;
+
+import java.util.Map.Entry;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -14,33 +18,34 @@ import java.util.stream.Collectors;
  *
  * Config Parameters -
  *
- *   fields (List<String>) : The list of fields to remove duplicates from
+ *   fieldMapping (Map<String, String>) : A mapping of fields to remove duplicates from and the field to output the result to.
  */
 public class RemoveDuplicateValues extends Stage {
 
-  private final List<String> fields;
+  private final Set<Entry<String, ConfigValue>> fieldMapping;
 
   public RemoveDuplicateValues(Config config) {
     super(config);
-    this.fields = config.getStringList("fields");
+    this.fieldMapping = config.getConfig("fields").entrySet();
   }
 
   @Override
   public void start() throws StageException {
-    if (fields.size() == 0)
+    if (fieldMapping.size() == 0)
       throw new StageException("Must supply at least one field to remove duplicate values from.");
   }
 
   @Override
   public List<Document> processDocument(Document doc) throws StageException {
-    for (String field : fields) {
+    for (Entry<String, ConfigValue> entry : fieldMapping) {
+      String field = entry.getKey();
+
       if (!doc.has(field) || !doc.isMultiValued(field)) {
         continue;
       }
 
       List<String> uniqueValues = doc.getStringList(field).stream().distinct().collect(Collectors.toList());
-      doc.removeField(field);
-      doc.update(field, UpdateMode.DEFAULT, uniqueValues.toArray(new String[0]));
+      doc.update((String) entry.getValue().unwrapped(), UpdateMode.DEFAULT, uniqueValues.toArray(new String[0]));
     }
 
     return null;
