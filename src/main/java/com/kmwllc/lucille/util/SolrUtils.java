dd5c661622b5a7e6ecb075f3f409a02ac811448b
Spencer Solomon <spencer.solomon@kmwllc.com>
2021-10-19 17:46:11 -0400

    Added documentation for SolrUtils

--- src/main/java/com/kmwllc/lucille/util/SolrUtils.java
+++ src/main/java/com/kmwllc/lucille/util/SolrUtils.java
@@ -5,8 +5,17 @@ import com.typesafe.config.Config;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 
+/**
+ * Utility methods for communicating with Solr.
+ */
 public class SolrUtils {
 
+  /**
+   * Generate a SolrClient from the given config file. Supports both Cloud and Http SolrClients.
+   *
+   * @param config  The configuration file to generate a client from
+   * @return  the solr client
+   */
   public static SolrClient getSolrClient(Config config) {
     if (config.hasPath("useCloudClient") && config.getBoolean("useCloudClient")) {
       return new CloudSolrClient.Builder(config.getStringList("solr.url")).build();
