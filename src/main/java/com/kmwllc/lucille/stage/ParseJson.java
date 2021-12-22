399012027c62b2c2420b4fdd420fe49058a211f6
Kyle Marcotte <kyle.marcotte@kmwllc.com>
2021-12-22 12:31:13 -0500

    LC-15 - create parse context in ParseJson.start and resuse.

--- src/main/java/com/kmwllc/lucille/stage/ParseJson.java
+++ src/main/java/com/kmwllc/lucille/stage/ParseJson.java
@@ -1,19 +1,14 @@
 package com.kmwllc.lucille.stage;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.*;
 import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
 import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 import com.kmwllc.lucille.core.Document;
 import com.kmwllc.lucille.core.Stage;
 import com.kmwllc.lucille.core.StageException;
 import com.typesafe.config.Config;
-
 
 import java.util.List;
 import java.util.Map;
@@ -22,12 +17,13 @@ import java.util.Map.Entry;
 /**
  * This stage parses a JSON string and sets fields on the processed document according to the configured mapping using
  * JsonPath expressions.
+ *
  * @see <a href="https://github.com/json-path/JsonPath">JsonPath</a>
  * Config Parameters
  * <p>
- *   - src (String) : The field containing the JSON string to be parsed.
- *   - jsonFieldPaths (Map<String, Object>) : Defines the mapping from JsonPath expressions
- *   to the destination fields in the processed document.
+ * - src (String) : The field containing the JSON string to be parsed.
+ * - jsonFieldPaths (Map<String, Object>) : Defines the mapping from JsonPath expressions
+ * to the destination fields in the processed document.
  * </p>
  */
 public class ParseJson extends Stage {
@@ -36,6 +32,7 @@ public class ParseJson extends Stage {
   private final String src;
   private final Map<String, Object> jsonFieldPaths;
   private final Configuration jsonPathConf;
+  private ParseContext jsonParseCtx;
 
   public ParseJson(Config config) {
     super(config);
@@ -49,26 +46,24 @@ public class ParseJson extends Stage {
 
   @Override
   public void start() {
-
+    this.jsonParseCtx = JsonPath.using(this.jsonPathConf);
   }
 
   /**
-   *
    * @param doc
    * @return
    */
   @Override
   public List<Document> processDocument(Document doc) throws StageException {
 
-      //Should this be a byte[]? Document doesn't support byte fields currently.
-      DocumentContext ctx = JsonPath.using(jsonPathConf).parse(doc.getString(this.src));
-      for (Entry<String, Object> entry : this.jsonFieldPaths.entrySet()) {
-        //JsonNode val = srcNode.at((String) entry.getValue());
-        JsonNode val = ctx.read((String) entry.getValue(), JsonNode.class);
+    DocumentContext ctx = this.jsonParseCtx.parse(doc.getString(this.src));
+    for (Entry<String, Object> entry : this.jsonFieldPaths.entrySet()) {
+      //JsonNode val = srcNode.at((String) entry.getValue());
+      JsonNode val = ctx.read((String) entry.getValue(), JsonNode.class);
 
-        doc.setField(entry.getKey(), val);
-      }
-      doc.removeField(this.src);
-      return null;
+      doc.setField(entry.getKey(), val);
+    }
+    doc.removeField(this.src);
+    return null;
   }
 }
