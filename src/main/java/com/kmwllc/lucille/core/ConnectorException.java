d35f25b88da2d2dadba9db3bb5d8a7b73f09a5b8
mmcintosh <mmcintosh@kmwllc.com>
2021-12-24 01:21:40 -0500

    LC-135 - Initial pass at S3 Connector with basic functionality
    The S3 connector allows you to connect to S3 and it publishes documents and Base64 encodes the content for passing through the system. The S3Connector should technically be named a VFSConnector because because it will also accept raw absolute and relative file paths as well as file:// schema. It needs tests and documentation still.

--- src/main/java/com/kmwllc/lucille/core/ConnectorException.java
+++ src/main/java/com/kmwllc/lucille/core/ConnectorException.java
@@ -9,6 +9,14 @@ public class ConnectorException extends Exception {
     super(message, cause);
   }
 
+  public ConnectorException(Throwable cause) {
+    super(cause);
+  }
+
+  protected ConnectorException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
   public ConnectorException() {
     super();
   }
