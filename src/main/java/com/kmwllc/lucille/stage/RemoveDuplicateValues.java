32fb12b1dba585fb2c2b3b3fd70983dfda9df50c
Daniel Jung <djung@kmwllc.com>
2022-02-18 08:43:02 -0800

    removed field mapping, removing duplicate values happens in-place

--- src/main/java/com/kmwllc/lucille/stage/RemoveDuplicateValues.java
+++ src/main/java/com/kmwllc/lucille/stage/RemoveDuplicateValues.java
@@ -16,37 +16,34 @@ import java.util.stream.Collectors;
 
 /**
  * This Stage removes duplicate values from the given list of fields.
- *
+ * <p>
  * Config Parameters -
- *
- *   fieldMapping (Map<String, String>) : A mapping of fields to remove duplicates from and the field to output the result to.
+ * <p>
+ * fields (List<String>) : A list of fields to remove duplicates from.
  */
 public class RemoveDuplicateValues extends Stage {
 
-  private final Map<String, Object> fieldMapping;
+  private final List<String> fields;
 
   public RemoveDuplicateValues(Config config) {
     super(config);
-    this.fieldMapping = config.getConfig("fieldMapping").root().unwrapped();
+    this.fields = config.getStringList("fields");
   }
 
   @Override
   public void start() throws StageException {
-    if (fieldMapping.size() == 0)
+    if (fields.size() == 0)
       throw new StageException("Must supply at least one field to remove duplicate values from.");
   }
 
   @Override
   public List<Document> processDocument(Document doc) throws StageException {
-    for (Entry<String, Object> entry : fieldMapping.entrySet()) {
-      String field = entry.getKey();
-
+    for (String field : fields) {
       if (!doc.has(field) || !doc.isMultiValued(field)) {
         continue;
       }
       doc.removeDuplicateValues(field);
     }
-
     return null;
   }
 }
