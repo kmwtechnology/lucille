973b44ef9f7568f2100d6eaf10f5ccdc23684c19
Daniel Jung <djung@kmwllc.com>
2022-03-15 10:42:47 -0700

    nested staff example

--- src/test/java/com/kmwllc/lucille/connector/XMLConnectorTest.java
+++ src/test/java/com/kmwllc/lucille/connector/XMLConnectorTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertTrue;
 public class XMLConnectorTest {
 
   @Test
-  public void testStaffExample() throws Exception {
+  public void testStaff() throws Exception {
     Config config = ConfigFactory.parseReader(FileUtils.getReader("classpath:XMLConnectorTest/staff.conf"));
     PersistingLocalMessageManager manager = new PersistingLocalMessageManager();
     Publisher publisher = new PublisherImpl(config, manager, "run1", "pipeline1");
@@ -25,6 +25,24 @@ public class XMLConnectorTest {
 
     List<Document> docs = manager.getSavedDocumentsSentForProcessing();
 
+    assertEquals(2, docs.size());
+
+    assertTrue(docs.get(0).has("xml"));
+    // ensure header is added to the xml
+    assertTrue(docs.get(0).getString("xml").startsWith("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"));
+  }
+
+  @Test
+  public void testNestedStaff() throws Exception {
+    Config config = ConfigFactory.parseReader(FileUtils.getReader("classpath:XMLConnectorTest/nestedstaff.conf"));
+    PersistingLocalMessageManager manager = new PersistingLocalMessageManager();
+    Publisher publisher = new PublisherImpl(config, manager, "run1", "pipeline1");
+    Connector connector = new XMLConnector(config);
+    connector.execute(publisher);
+
+    List<Document> docs = manager.getSavedDocumentsSentForProcessing();
+
+    // ensure that in a nested scenario, the nested tag does not get included
     assertEquals(2, docs.size());
 
     assertTrue(docs.get(0).has("xml"));
