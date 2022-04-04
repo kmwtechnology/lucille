96ea274752ca34569284d8e6f41918bf8c5c35ff
Daniel Jung <djung@kmwllc.com>
2022-04-04 09:36:43 -0700

    joint test

--- src/test/java/com/kmwllc/lucille/connector/XMLConnectorTest.java
+++ src/test/java/com/kmwllc/lucille/connector/XMLConnectorTest.java
@@ -154,7 +154,7 @@ public class XMLConnectorTest {
   }
 
   @Test
-  public void jointTest() throws Exception {
+  public void WithXPathExtractorTest() throws Exception {
     Config config = ConfigFactory.parseReader(FileUtils.getReader("classpath:XMLConnectorTest/staff.conf"));
     PersistingLocalMessageManager manager = new PersistingLocalMessageManager();
     Publisher publisher = new PublisherImpl(config, manager, "run1", "pipeline1");
