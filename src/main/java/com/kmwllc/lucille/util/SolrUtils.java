4f0fc111e098b57c9dfda81d037eab50b4f5f818
Daniel Jung <djung@kmwllc.com>
2022-04-11 12:57:49 -0700

    updated method

--- src/main/java/com/kmwllc/lucille/util/SolrUtils.java
+++ src/main/java/com/kmwllc/lucille/util/SolrUtils.java
@@ -3,6 +3,7 @@ package com.kmwllc.lucille.util;
 
 import com.kmwllc.lucille.core.Document;
 import com.kmwllc.lucille.core.DocumentException;
+import com.kmwllc.lucille.stage.XPathExtractor;
 import com.typesafe.config.Config;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -10,6 +11,8 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
@@ -23,6 +26,8 @@ import java.util.Map;
  */
 public class SolrUtils {
 
+  private static final Logger log = LogManager.getLogger(SolrUtils.class);
+
   /**
    * Generate a SolrClient from the given config file. Supports both Cloud and Http SolrClients.
    *
@@ -31,16 +36,18 @@ public class SolrUtils {
    */
   public static SolrClient getSolrClient(Config config) {
     String solrUrl = getSolrUrl(config);
-    CredentialsProvider provider = new BasicCredentialsProvider();
-    System.out.println(requiresAuth(config));
+    HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+
     if (requiresAuth(config)) {
+      CredentialsProvider provider = new BasicCredentialsProvider();
       String userName = config.getString("solr.userName");
       String password = config.getString("solr.password");
       UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(userName, password);
       provider.setCredentials(AuthScope.ANY, credentials);
+      clientBuilder.setDefaultCredentialsProvider(provider);
+      clientBuilder.addInterceptorFirst(new PreemptiveAuthInterceptor());
     }
-    HttpClientBuilder clientBuilder = HttpClientBuilder.create().setDefaultCredentialsProvider(provider);
-    clientBuilder.addInterceptorFirst(new PreemptiveAuthInterceptor());
+
     HttpClient client = clientBuilder.build();
     if (config.hasPath("useCloudClient") && config.getBoolean("useCloudClient")) {
       return requiresAuth(config) ? new CloudSolrClient.Builder(getSolrUrls(config)).withHttpClient(client).build() : new CloudSolrClient.Builder(getSolrUrls(config)).build();
@@ -49,11 +56,13 @@ public class SolrUtils {
     }
   }
 
-
-
-
   public static boolean requiresAuth(Config config) {
-    return config.hasPath("solr.userName") && config.hasPath("solr.password");
+    boolean hasUserName = config.hasPath("solr.userName");
+    boolean hasPassword = config.hasPath("solr.password");
+    if (hasUserName != hasPassword) {
+      log.error("Both the userName and password must be set.");
+    }
+    return hasUserName && hasPassword;
   }
 
   public static String getSolrUrl(Config config) {
