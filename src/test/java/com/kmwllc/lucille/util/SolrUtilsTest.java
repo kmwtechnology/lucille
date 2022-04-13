537243d579147b74e4085062dea2359bb7ed6cae
Daniel Jung <djung@kmwllc.com>
2022-04-13 07:32:09 -0700

    added nonnull test

--- src/test/java/com/kmwllc/lucille/util/SolrUtilsTest.java
+++ src/test/java/com/kmwllc/lucille/util/SolrUtilsTest.java
@@ -1,26 +1,25 @@
 package com.kmwllc.lucille.util;
 
-import com.kmwllc.lucille.core.ConfigUtils;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import org.apache.commons.io.IOUtils;
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.http.client.HttpClient;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.Reader;
-import java.net.http.HttpClient;
+import static org.junit.Assert.*;
 
-import static com.kmwllc.lucille.util.SolrUtils.getSolrClient;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class SolrUtilsTest {
   @Test
   public void requireAuthTest() throws Exception {
     Config config = ConfigFactory.parseReader(FileUtils.getReader("classpath:SolrUtilsTest/auth.conf"));
-
     assertTrue(SolrUtils.requiresAuth(config));
+  }
+
+
+  @Test
+  public void getHttpClientTest() throws Exception {
+    Config config = ConfigFactory.parseReader(FileUtils.getReader("classpath:SolrUtilsTest/auth.conf"));
+    HttpClient client = SolrUtils.getHttpClient(config);
+    assertNotNull(client);
   }
 }
