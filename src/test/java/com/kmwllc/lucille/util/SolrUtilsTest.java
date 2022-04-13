ee71bef05a57fd21f59bdec8c498944cb2b77a48
Daniel Jung <djung@kmwllc.com>
2022-04-13 11:45:43 -0700

    added comment

--- src/test/java/com/kmwllc/lucille/util/SolrUtilsTest.java
+++ src/test/java/com/kmwllc/lucille/util/SolrUtilsTest.java
@@ -20,6 +20,7 @@ public class SolrUtilsTest {
   public void getHttpClientTest() throws Exception {
     Config config = ConfigFactory.parseReader(FileUtils.getReader("classpath:SolrUtilsTest/auth.conf"));
     HttpClient client = SolrUtils.getHttpClient(config);
+    // would like to inspect the solr client to confirm credentials are configured, but can’t do that so just checking it’s non-null
     assertNotNull(client);
   }
 }
