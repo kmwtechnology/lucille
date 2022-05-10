9eaf4c5d6c5130733b88e3038d63067c148f0ee6
Rudi Seitz <rudi@kmwllc.com>
2022-05-10 16:46:29 -0400

    Update Heartbeat test to check the line count of the heartbeat log rather than deleting the file.

--- src/test/java/com/kmwllc/lucille/core/HeartbeatTest.java
+++ src/test/java/com/kmwllc/lucille/core/HeartbeatTest.java
@@ -7,38 +7,47 @@ import com.typesafe.config.ConfigFactory;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertTrue;
 
 public class HeartbeatTest {
+
   @Test
   public void testWatcher() throws Exception {
+
+    // We might like to delete the heartbeat log and see if it is recreated.
+    // But that's not safe to do. The logging system is initialized once for the whole test run
+    // and may not recover if a logfile that was written in a previous test gets
+    // unexpectedly deleted here. So instead we check that the line count of the heartbeat log
+    // increases.
     File heartbeatLog = new File("log/heartbeat.log");
+    long previousLineCount = 0;
     if (heartbeatLog.exists()) {
-      heartbeatLog.delete();
+      previousLineCount = Files.lines(heartbeatLog.toPath()).count();
     }
 
     Config config = ConfigFactory.load("WorkerPoolTest/watcher.conf");
-    WorkerPool pool1 = new WorkerPool(config, "pipeline1", WorkerMessageManagerFactory.getConstantFactory(new LocalMessageManager()), "");
+    WorkerPool pool1 = new WorkerPool(config, "pipeline1",
+      WorkerMessageManagerFactory.getConstantFactory(new LocalMessageManager()), "");
 
     pool1.start();
 
-    // sleep for more than 1 second
+    // give the worker time to begin generating heartbeats
     Thread.sleep(3000);
 
     pool1.stop();
 
     assertTrue(heartbeatLog.exists());
 
-    Scanner scanner = new Scanner(heartbeatLog);
-    while (scanner.hasNextLine()) {
-      String heartbeat = scanner.nextLine();
-      assertTrue(heartbeat.contains("INFO Heartbeat: Issuing heartbeat"));
-    }
+    Stream<String> lines = Files.lines(heartbeatLog.toPath());
+    long currentLineCount = Files.lines(heartbeatLog.toPath()).count();
 
-    if (heartbeatLog.exists()) {
-      heartbeatLog.delete();
-    }
+    assertTrue(currentLineCount > previousLineCount);
+
+    String lastLine = lines.skip(currentLineCount - 1).findFirst().get();
+    assertTrue(lastLine.contains("INFO Heartbeat: Issuing heartbeat"));
   }
+
 }
