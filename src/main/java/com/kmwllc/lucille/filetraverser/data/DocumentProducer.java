bbbed29e157d14694b13b21cbe0cd577420fe6ce
Jake Horban <jake@kmwllc.com>
2022-08-10 11:36:28 +0200

    remove comment

--- src/main/java/com/kmwllc/lucille/filetraverser/data/DocumentProducer.java
+++ src/main/java/com/kmwllc/lucille/filetraverser/data/DocumentProducer.java
@@ -82,8 +82,6 @@ public interface DocumentProducer {
   }
 
   /**
-   *
-   * todo why is this not recognized
    * Creates a tombstone of the provided document if an error occurs, logging the error in the
    * {@link Document#ERROR_FIELD} and ensuring the {@link Document#ID_FIELD} and {@link FileTraverser#FILE_PATH} fields
    * are set.
