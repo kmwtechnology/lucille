52944c51012c36412a4f59d55cb9b8b333859f48
Jake Horban <jake@kmwllc.com>
2022-10-14 12:08:03 -0400

    add tests to demonstrate nested array issue

--- src/test/java/com/kmwllc/lucille/core/HashMapDocumentTest.java
+++ src/test/java/com/kmwllc/lucille/core/HashMapDocumentTest.java
@@ -2,8 +2,12 @@ package com.kmwllc.lucille.core;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
 
+import java.util.List;
 import java.util.function.UnaryOperator;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class HashMapDocumentTest extends DocumentTest {
@@ -26,5 +30,12 @@ public class HashMapDocumentTest extends DocumentTest {
   @Override
   public Document createDocumentFromJson(String json, UnaryOperator<String> idUpdater) throws DocumentException, JsonProcessingException {
     return HashMapDocument.fromJsonString(json, idUpdater);
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testNestedArrays() throws DocumentException, JsonProcessingException {
+    // todo decide how this should be addressed
+    Document d = createDocumentFromJson("{\"id\":\"id\",\"nested\":[[\"first\"],[\"second\"]]}");
+    assertEquals(List.of(List.of("first"), List.of("second")), d.getStringList("nested"));
   }
 }
--- src/test/java/com/kmwllc/lucille/core/JsonDocumentTest.java
+++ src/test/java/com/kmwllc/lucille/core/JsonDocumentTest.java
@@ -2,8 +2,12 @@ package com.kmwllc.lucille.core;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
 
+import java.util.List;
 import java.util.function.UnaryOperator;
+
+import static org.junit.Assert.assertEquals;
 
 public class JsonDocumentTest extends DocumentTest.NodeDocumentTest {
 
@@ -25,5 +29,13 @@ public class JsonDocumentTest extends DocumentTest.NodeDocumentTest {
   @Override
   public Document createDocumentFromJson(String json, UnaryOperator<String> idUpdater) throws DocumentException, JsonProcessingException {
     return JsonDocument.fromJsonString(json, idUpdater);
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testNestedArrays() throws DocumentException, JsonProcessingException {
+    // todo decide how this should be addressed
+    Document d = createDocumentFromJson("{\"id\":\"id\",\"nested\":[[\"first\"],[\"second\"]]}");
+    List<String> field = d.getStringList("nested"); // this returns a list with two empty strings
+    assertEquals(List.of(List.of("first"), List.of("second")), d.getStringList("nested"));
   }
 }
