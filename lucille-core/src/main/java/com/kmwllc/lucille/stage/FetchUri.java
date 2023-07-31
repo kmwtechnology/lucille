1735220a8722f7a88237e4eb06f61cbe550c0ec2
abijitran <abijit@kmwllc.com>
2023-07-31 14:37:52 -0400

    added config file with appropriate field mappings, test with mocking, and appropriate code for stage

--- lucille-core/src/main/java/com/kmwllc/lucille/stage/FetchUri.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/stage/FetchUri.java
@@ -5,18 +5,19 @@ import com.kmwllc.lucille.core.Stage;
 import com.kmwllc.lucille.core.StageException;
 import com.typesafe.config.Config;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
 import java.util.Map;
 
 import java.util.Map.Entry;
-import java.util.Scanner;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class FetchUri extends Stage {
 
@@ -26,23 +27,52 @@ public class FetchUri extends Stage {
   public FetchUri(Config config) {
     super(config, new StageSpec().withRequiredParents("fieldMapping"));
     this.fieldMap = config.getConfig("fieldMapping").root().unwrapped();
+    client = HttpClients.createDefault();
+  }
+
+  void setClient(CloseableHttpClient newClient) {
+    this.client = newClient;
   }
 
   @Override
   public Iterator<Document> processDocument(Document doc) throws StageException {
     for (Entry<String, Object> e : fieldMap.entrySet()) {
-      final HttpGet httpGet = new HttpGet(doc.getString(e.getKey()));
-      try (CloseableHttpClient client = HttpClients.createDefault();) {
-        HttpResponse httpResponse = client.execute(httpGet);
-        byte[] bytes = IOUtils.toByteArray(httpResponse.getEntity().getContent());
-        doc.setField((String) e.getValue(), bytes);
+      String fieldName = e.getKey();
+      String destinationFieldName = (String) e.getValue();
+      String uri = doc.getString(fieldName);
+
+      HttpGet httpGet = new HttpGet(uri);
+      CloseableHttpResponse httpResponse = null;
+      try {
+        httpResponse = client.execute(httpGet);
+        HttpEntity ent = httpResponse.getEntity();
+        InputStream content = ent.getContent();
+        byte[] bytes = IOUtils.toByteArray(content);
+        doc.setField(destinationFieldName, bytes);
+        EntityUtils.consume(ent);
       } catch (ClientProtocolException ex) {
-        throw new RuntimeException(ex);
+        throw new StageException("Error processing GET", ex);
       } catch (IOException ex) {
-        throw new RuntimeException(ex);
+        throw new StageException("Error processing GET", ex);
+      } finally {
+        try {
+          httpResponse.close();
+        } catch (IOException ex) {
+          throw new StageException("Error processing GET", ex);
+        }
       }
     }
 
     return null;
   }
+
+  @Override
+  public void stop() throws StageException {
+    try {
+      client.close();
+    } catch (IOException e) {
+      throw new StageException("Error closing client", e);
+    }
+  }
+
 }
--- lucille-core/src/test/java/com/kmwllc/lucille/stage/FetchUriTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/stage/FetchUriTest.java
@@ -1,5 +1,57 @@
 package com.kmwllc.lucille.stage;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import com.kmwllc.lucille.core.Document;
+import com.kmwllc.lucille.core.StageException;
+import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
 public class FetchUriTest {
+
+  private CloseableHttpClient mockClient;
+
+  @Before
+  public void setup() {
+    mockClient = Mockito.mock(CloseableHttpClient.class);
+    CloseableHttpResponse mockResponse = Mockito.mock(CloseableHttpResponse.class);
+    HttpEntity mockEntity =  Mockito.mock(HttpEntity.class);
+
+    try {
+      Mockito.when(mockClient.execute(Mockito.any(HttpGet.class))).thenReturn(mockResponse);
+      Mockito.when(mockResponse.getEntity()).thenReturn(mockEntity);
+      Mockito.when(mockEntity.getContent()).thenReturn(IOUtils.toInputStream("exampleresponse", "UTF-8"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  @Test
+  public void testFetchUri() throws StageException {
+    FetchUri s = (FetchUri) StageFactory.of(FetchUri.class).get("FetchUriTest/config.conf");
+    s.setClient(mockClient);
+    Document d = Document.create("id");
+    d.setField("name", "Jane Doe"); // extra field to test that not all fields are being read
+    d.setField("url", "https://example.com"); // uri field that is meant to be read
+
+    s.processDocument(d);
+
+    byte[] expectedResult;
+    try {
+      expectedResult = IOUtils.toByteArray(IOUtils.toInputStream("exampleresponse", "UTF-8"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    assertArrayEquals(expectedResult, d.getBytes("url_data"));
+  }
 
 }
