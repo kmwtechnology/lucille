aef8026dba94e6e9f00903e25d8407d942953080
abijitran <abijit@kmwllc.com>
2023-08-16 11:51:57 -0400

    removed extraneous code in fileutilstest

--- lucille-core/src/test/java/com/kmwllc/lucille/util/FileUtilsTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/util/FileUtilsTest.java
@@ -1,25 +1,12 @@
 package com.kmwllc.lucille.util;
-
 import org.apache.commons.io.IOUtils;
-import org.junit.Before;
 import org.junit.Test;
-
 import java.io.File;
 import java.io.Reader;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 
 public class FileUtilsTest {
-
-  @Before
-  public void setup() {
-//    try (MockedStatic<VFSInputStream> utilities = Mockito.mockStatic(VFSInputStream.class)) {
-//      utilities.when(VFSInputStream::open).thenReturn("Eugen");
-//      assertEquals(VFSInputStream.open()).isEqualTo("Eugen");
-//    }
-  }
 
   @Test
   public void getReaderTest() throws Exception {
