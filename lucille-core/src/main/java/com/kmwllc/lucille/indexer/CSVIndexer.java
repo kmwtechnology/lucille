4c063d51c68ff02daeb6c056074a867f66bb6da6
Rudi Seitz <rudi@kmwllc.com>
2023-08-30 13:34:15 -0400

    add Dockerfile; update CSVIndexer to create parent path for dest file

--- lucille-core/src/main/java/com/kmwllc/lucille/indexer/CSVIndexer.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/indexer/CSVIndexer.java
@@ -6,6 +6,7 @@ import com.kmwllc.lucille.message.IndexerMessageManager;
 import com.opencsv.CSVWriterBuilder;
 import com.opencsv.ICSVWriter;
 import com.typesafe.config.Config;
+import java.io.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,7 +43,11 @@ public class CSVIndexer extends Indexer {
   private static ICSVWriter getCsvWriter(Config config, boolean bypass) {
     boolean append = config.hasPath("csv.append") ? config.getBoolean("csv.append") : false;
     try {
-      CSVWriterBuilder builder = new CSVWriterBuilder(new FileWriter(config.getString("csv.path"), append));
+      File file = new File(config.getString("csv.path")).getAbsoluteFile();
+      if (!file.getParentFile().exists()) {
+        file.getParentFile().mkdirs();
+      }
+      CSVWriterBuilder builder = new CSVWriterBuilder(new FileWriter(file, append));
       return builder.build();
     } catch (IOException e) {
       log.error("Error initializing writer for CSVIndexer", e);
