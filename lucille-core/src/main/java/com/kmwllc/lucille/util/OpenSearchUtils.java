095a66af355ced109c2bb969ac6c3a603afed3ad
abijitran <abijit@kmwllc.com>
2023-09-15 09:26:18 -0400

    Changed to apache http client

--- lucille-core/src/main/java/com/kmwllc/lucille/util/OpenSearchUtils.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/util/OpenSearchUtils.java
@@ -2,19 +2,23 @@ package com.kmwllc.lucille.util;
 
 import com.typesafe.config.Config;
 import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLEngine;
 import nl.altindag.ssl.SSLFactory;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
-import org.opensearch.client.RestClient;
-import org.opensearch.client.RestClientBuilder.HttpClientConfigCallback;
-import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.core5.function.Factory;
+import org.apache.hc.core5.http.nio.ssl.TlsStrategy;
+import org.apache.hc.core5.reactor.ssl.TlsDetails;
+import org.apache.hc.client5.http.auth.AuthScope;
+import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.transport.OpenSearchTransport;
-import org.opensearch.client.transport.rest_client.RestClientTransport;
+import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBuilder;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
 
 /**
  * Utility methods for communicating with OpenSearch.
@@ -33,16 +37,17 @@ public class OpenSearchUtils {
     URI hostUri = URI.create(getOpenSearchUrl(config));
 
     //Establish credentials to use basic authentication.
-    final CredentialsProvider provider = new BasicCredentialsProvider();
+    final BasicCredentialsProvider provider = new BasicCredentialsProvider();
 
     // get user info from URI if present and setup BasicAuth credentials if needed
     String userInfo = hostUri.getUserInfo();
+    HttpHost host = new HttpHost(hostUri.getScheme(), hostUri.getHost(), hostUri.getPort());
     if (userInfo != null) {
       int pos = userInfo.indexOf(":");
       String username = userInfo.substring(0, pos);
       String password = userInfo.substring(pos + 1);
-      provider.setCredentials(AuthScope.ANY,
-          new UsernamePasswordCredentials(username, password));
+      provider.setCredentials(new AuthScope(host),
+          new UsernamePasswordCredentials(username, password.toCharArray()));
     }
 
     // needed to allow for local testing of HTTPS
@@ -51,23 +56,55 @@ public class OpenSearchUtils {
     if (allowInvalidCert) {
       sslFactoryBuilder
           .withTrustingAllCertificatesWithoutValidation()
-          .withHostnameVerifier((host, session) -> true);
+          .withHostnameVerifier((sampleHost, sampleSession) -> true);
     } else {
       sslFactoryBuilder.withDefaultTrustMaterial();
     }
 
     SSLFactory sslFactory = sslFactoryBuilder.build();
 
-    RestClient restClient = RestClient.builder(new HttpHost(hostUri.getHost(), hostUri.getPort(), hostUri.getScheme()))
-        .setHttpClientConfigCallback(new HttpClientConfigCallback() {
-          @Override
-          public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-            return httpClientBuilder.setDefaultCredentialsProvider(provider).setSSLContext(sslFactory.getSslContext())
-                .setSSLHostnameVerifier(sslFactory.getHostnameVerifier());
-          }
-        }).build();
+//    RestClient restClient = RestClient.builder(new HttpHost(hostUri.getHost(), hostUri.getPort(), hostUri.getScheme()))
+//        .setHttpClientConfigCallback(new HttpClientConfigCallback() {
+//          @Override
+//          public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+//            return httpClientBuilder.setDefaultCredentialsProvider(provider).setSSLContext(sslFactory.getSslContext())
+//                .setSSLHostnameVerifier(sslFactory.getHostnameVerifier());
+//          }
+//        }).build();
+//
+//    final OpenSearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+//    final OpenSearchClient client = new OpenSearchClient(transport);
+//
+//    return client;
 
-    final OpenSearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+
+    ApacheHttpClient5TransportBuilder builder = ApacheHttpClient5TransportBuilder.builder(host);
+    builder.setHttpClientConfigCallback(httpClientBuilder -> {
+      final TlsStrategy tlsStrategy = ClientTlsStrategyBuilder.create()
+          //.setSslContext(SSLContextBuilder.create().build())
+          .setSslContext(sslFactory.getSslContext())
+          .setTlsDetailsFactory(new Factory<SSLEngine, TlsDetails>() {
+            @Override
+            public TlsDetails create(final SSLEngine sslEngine) {
+              return new TlsDetails(sslEngine.getSession(), sslEngine.getApplicationProtocol());
+            }
+  //            @Override
+  //            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+  //              return httpClientBuilder.setDefaultCredentialsProvider(provider).setSSLContext()
+  //                  .setSSLHostnameVerifier(sslFactory.getHostnameVerifier());
+  //            }
+        }).build();
+      final PoolingAsyncClientConnectionManager connectionManager = PoolingAsyncClientConnectionManagerBuilder
+          .create()
+          .setTlsStrategy(tlsStrategy)
+          .build();
+
+      return httpClientBuilder
+          .setDefaultCredentialsProvider(provider)
+          .setConnectionManager(connectionManager);
+    });
+
+    final OpenSearchTransport transport = ApacheHttpClient5TransportBuilder.builder(host).build();
     final OpenSearchClient client = new OpenSearchClient(transport);
 
     return client;
