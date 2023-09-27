2c4bb20704a45963747bc12f77a70c9551e1a2fe
abijitran <abijit@kmwllc.com>
2023-09-27 15:42:11 -0400

    Added tests to test both minmax and cardinality settings

--- lucille-core/src/test/java/com/kmwllc/lucille/stage/AddRandomFieldTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/stage/AddRandomFieldTest.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 import com.kmwllc.lucille.stage.StageFactory;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 
 public class AddRandomFieldTest {
@@ -35,16 +37,16 @@ public class AddRandomFieldTest {
       stage.processDocument(doc);
     }
 
-    int docVal1 = Integer.valueOf(doc1.getStringList("data"));
-    int docVal2 = Integer.valueOf(doc2.getStringList("data"));
-    int docVal3 = Integer.valueOf(doc3.getStringList("data"));
+    int docVal1 = Integer.valueOf(doc1.getString("data"));
+    int docVal2 = Integer.valueOf(doc2.getString("data"));
+    int docVal3 = Integer.valueOf(doc3.getString("data"));
     assertTrue(0 < docVal1 && docVal1 < 20);
     assertTrue(0 < docVal2 && docVal2 < 20);
     assertTrue(0 < docVal3 && docVal3 < 20);
   }
 
   @Test
-  public void testBasicFilePath() throws StageException {
+  public void testBasicFilePath() throws StageException, IOException {
     Stage stage = factory.get("AddRandomFieldTest/basicfilepath.conf");
     Document doc1 = Document.create("doc1");
     Document doc2 = Document.create("doc2");
@@ -54,17 +56,55 @@ public class AddRandomFieldTest {
       stage.processDocument(doc);
     }
 
+    List<String> data = Files.readAllLines(Path.of("src/test/resources/AddRandomFieldTest/foods.txt"));
+
     String docVal1 = doc1.getString("data");
     String docVal2 = doc2.getString("data");
     String docVal3 = doc3.getString("data");
     NumberFormatException thrown = assertThrows(NumberFormatException.class, () -> Integer.valueOf(docVal1));
     assertTrue(thrown.getLocalizedMessage().startsWith("For input string"));
-
-    // TODO: load in foods.txt and check that the values exist as part of the arraylist from file
+    assertTrue(data.contains(docVal1));
+    assertTrue(data.contains(docVal2));
+    assertTrue(data.contains(docVal3));
   }
 
-  // TODO:
-  // - test to validate that cardinality works
-  // - test to validate that field name works
-  // - test to validate that min
+  @Test
+  public void testCardinality() throws StageException, IOException {
+    Stage stage = factory.get("AddRandomFieldTest/basicfilepath.conf");
+    Document doc1 = Document.create("doc1");
+    Document doc2 = Document.create("doc2");
+    Document doc3 = Document.create("doc3");
+
+    for (Document doc : Arrays.asList(doc1, doc2, doc3)) {
+      stage.processDocument(doc);
+    }
+
+    List<String> docVal1 = doc1.getStringList("data");
+    List<String> docVal2 = doc2.getStringList("data");
+    List<String> docVal3 = doc3.getStringList("data");
+
+    for (List<String> docVal : Arrays.asList(docVal1, docVal2, docVal3)) {
+      assertTrue(docVal.stream().allMatch(s -> s.equals(docVal.get(0))));
+    }
+  }
+
+  @Test
+  public void testMinMax() throws StageException, IOException {
+    Stage stage = factory.get("AddRandomFieldTest/basicfilepath.conf");
+    Document doc1 = Document.create("doc1");
+    Document doc2 = Document.create("doc2");
+    Document doc3 = Document.create("doc3");
+
+    for (Document doc : Arrays.asList(doc1, doc2, doc3)) {
+      stage.processDocument(doc);
+    }
+
+    List<String> docVal1 = doc1.getStringList("data");
+    List<String> docVal2 = doc2.getStringList("data");
+    List<String> docVal3 = doc3.getStringList("data");
+
+    for (List<String> docVal : Arrays.asList(docVal1, docVal2, docVal3)) {
+      assertTrue(docVal.size() == 10);
+    }
+  }
 }
