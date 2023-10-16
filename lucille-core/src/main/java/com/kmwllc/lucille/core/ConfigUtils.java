c7cfe54611605ef9e80fd9776264410a2d52b08e
abijitran <abijit@kmwllc.com>
2023-10-16 16:39:00 -0400

    removed loadConfig() method

--- lucille-core/src/main/java/com/kmwllc/lucille/core/ConfigUtils.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/core/ConfigUtils.java
@@ -11,27 +11,7 @@ public class ConfigUtils {
   public static final String ENV_PROP = "pipeline.env";
 
   private static final Logger log = LoggerFactory.getLogger(ConfigUtils.class);
-
-  public static Config loadConfig() throws ConfigException {
-
-    String envName = System.getProperty(ENV_PROP);
-
-    Config originalConfig = ConfigFactory.load();
-
-    if (envName == null) {
-      log.info("pipeline.env system property not set");
-      return originalConfig;
-    }
-
-    log.info("pipeline.env system property = " + envName);
-    // Override default settings in application.conf with settings for the named environment.
-    // For example, if application.conf contains x=1 and dev.x=2, and if the
-    // pipeline.env system property is set to "dev", then accessing "x" will return 2
-    // even though the "dev" prefix was not provided when accessing "x"
-    return originalConfig.getConfig(envName).withFallback(originalConfig);
-
-  }
-
+  
   /**
    * Get the value of the given setting from the config file, or a default value if the setting does not exist in the
    * config.
