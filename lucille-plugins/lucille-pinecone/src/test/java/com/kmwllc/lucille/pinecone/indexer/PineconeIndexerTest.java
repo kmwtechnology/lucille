eec2f9fa028272600591e537766a880a1897f136
Akul Sethi <akul@kmwllc.com>
2024-01-23 15:30:16 -0500

    unit tests pinecone indexer

--- lucille-plugins/lucille-pinecone/src/test/java/com/kmwllc/lucille/pinecone/indexer/PineconeIndexerTest.java
+++ lucille-plugins/lucille-pinecone/src/test/java/com/kmwllc/lucille/pinecone/indexer/PineconeIndexerTest.java
@@ -1,15 +1,19 @@
 package com.kmwllc.lucille.pinecone.indexer;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.theories.suppliers.TestedOn;
 import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 import org.mockito.internal.matchers.Any;
 import org.mockito.invocation.InvocationOnMock;
@@ -23,6 +27,7 @@ import com.typesafe.config.ConfigValue;
 import android.annotation.TargetApi;
 import io.grpc.ConnectivityState;
 import io.pinecone.PineconeClient;
+import io.pinecone.PineconeClientConfig;
 import io.pinecone.PineconeConnection;
 import io.pinecone.PineconeConnectionConfig;
 import io.pinecone.proto.UpdateRequest;
@@ -62,6 +67,31 @@ public class PineconeIndexerTest {
     Mockito.when(mockClient.connect("failure")).thenReturn(transientFailureConnection);
     Mockito.when(mockClient.connect("shutdown")).thenReturn(shutdownConnection);
 
+  }
+  
+  @Test 
+  public void testClientCreatedWithCorrectConfig() {
+    Map<PineconeClient, List<Object>> constructorArgs = new HashMap<>();
+
+    try(MockedConstruction<PineconeClient> client = Mockito.mockConstruction(PineconeClient.class, (mock, context) -> {
+      constructorArgs.put(mock, new ArrayList<>(context.arguments()));
+    })) {
+      Config configGood = ConfigFactory.load("PineconeIndexerTest/good-config.conf");
+      TestMessenger messenger = new TestMessenger();
+      new PineconeIndexer(configGood, messenger, "testing");
+
+      assertTrue(client.constructed().size() == 1);
+      PineconeClient constructed = client.constructed().get(0);
+      assertTrue(constructorArgs.get(constructed) instanceof PineconeClientConfig);
+
+      PineconeClientConfig config = (PineconeClientConfig) constructorArgs.get(constructed);
+
+      assertEquals("apiKey", config.getApiKey());
+      assertEquals("enviroment", config.getEnvironment());
+      assertEquals("projectName", config.getProjectName());
+      assertEquals(100, config.getServerSideTimeoutSec());
+
+    }
 
   }
 
@@ -102,7 +132,7 @@ public class PineconeIndexerTest {
 
 
   @Test
-  public void sendToIndexUpsert() throws Exception {
+  public void testUpsertNoNamespacesProvided() throws Exception {
     ArgumentCaptor<UpdateRequest> updateRequest = ArgumentCaptor.forClass(UpdateRequest.class);
     Mockito.verify(stub.update(updateRequest.capture()));
     ArgumentCaptor<UpsertRequest> upsertRequest = ArgumentCaptor.forClass(UpsertRequest.class);
@@ -117,19 +147,51 @@ public class PineconeIndexerTest {
     Document doc0 = Document.create("doc0");
     Document doc1 = Document.create("doc1");
     doc0.update("doc0", UpdateMode.OVERWRITE, 1.0, 2.0);
-    doc0.update("content", UpdateMode.APPEND, 1.0);
-    doc0.update("content", UpdateMode.APPEND, 2.0);
-    doc1.update("content", UpdateMode.APPEND, 3.0);
-    doc1.update("content", UpdateMode.APPEND, 4.0);
+    doc1.update("doc1", UpdateMode.OVERWRITE, 3.0, 4.0);
 
     messenger.sendForIndexing(doc0);
     messenger.sendForIndexing(doc1);
     indexerGood.run(2);
 
 
+    //make sure no upserts or updates have happened 
     assertTrue(upsertRequest.getAllValues().isEmpty());
     assertTrue(updateRequest.getAllValues().isEmpty());
   }
+
+  @Test
+  public void testUpsertTwoNamespace() throws Exception {
+    ArgumentCaptor<UpdateRequest> updateRequest = ArgumentCaptor.forClass(UpdateRequest.class);
+    Mockito.verify(stub.update(updateRequest.capture()));
+    ArgumentCaptor<UpsertRequest> upsertRequest = ArgumentCaptor.forClass(UpsertRequest.class);
+    Mockito.verify(stub.upsert(upsertRequest.capture()));
+
+    TestMessenger messenger = new TestMessenger();
+    Config configGood = ConfigFactory.load("PineconeIndexerTest/two-namespaces.conf");
+
+    PineconeIndexer indexerGood = new PineconeIndexer(configGood, messenger, "testing");
+
+    indexerGood.validateConnection();
+    Document doc0 = Document.create("doc0");
+    Document doc1 = Document.create("doc1");
+    doc0.update("vector", UpdateMode.OVERWRITE, 1.0, 2.0);
+    doc0.update("metaString1", UpdateMode.OVERWRITE, "some string data");
+    doc0.update("metaString2", UpdateMode.OVERWRITE, "some more string data");
+    doc0.update("metaList", UpdateMode.OVERWRITE, 1, 2, 3);
+    doc1.update("vector", UpdateMode.OVERWRITE, 3.0, 4.0);
+    doc1.update("metaString1", UpdateMode.OVERWRITE, "some string data");
+    doc1.update("metaString2", UpdateMode.OVERWRITE, "some more string data");
+    doc1.update("metaList", UpdateMode.OVERWRITE, 1, 2, 3);
+
+    messenger.sendForIndexing(doc0);
+    messenger.sendForIndexing(doc1);
+    indexerGood.run(2);
+
+    //make sure no upserts or updates have happened 
+    assertTrue(upsertRequest.getAllValues().isEmpty());
+    assertTrue(updateRequest.getAllValues().isEmpty());
+  }
+
 
   
 
