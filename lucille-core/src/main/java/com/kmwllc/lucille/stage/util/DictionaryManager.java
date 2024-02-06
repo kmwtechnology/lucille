68f2161a21eeb439fb631a6d6da8425010ca0724
Kira Traynor <k2traynor@gmail.com>
2024-02-05 18:12:50 -0700

    Fixes bug in log statement that was changed from using String.format.

--- lucille-core/src/main/java/com/kmwllc/lucille/stage/util/DictionaryManager.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/stage/util/DictionaryManager.java
@@ -116,8 +116,8 @@ public class DictionaryManager {
 
         for (String term : line) {
           if (term.contains("\uFFFD")) {
-            log.warn(String.format("Entry \"%s\" on line %d contained malformed characters which were removed. " +
-                "This dictionary entry will be ignored.", term, reader.getLinesRead()));
+            log.warn("Entry \"{}\" on line {} contained malformed characters which were removed. " +
+                "This dictionary entry will be ignored.", term, reader.getLinesRead());
             ignore = true;
             break;
           }
