fade90de56d7ccd50b17a2469e90a2915dba0802
Akul Sethi <akul@kmwllc.com>
2024-02-29 14:35:53 -0500

    log warning added

--- lucille-core/src/main/java/com/kmwllc/lucille/stage/ParseFloats.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/stage/ParseFloats.java
@@ -9,10 +9,13 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ParseFloats extends Stage {
 
   private final String field;
+  private static final Logger log = LogManager.getLogger(ParseFloats.class);
 
   public ParseFloats(Config config) {
     super(config, new StageSpec().withRequiredProperties("field"));
@@ -28,6 +31,7 @@ public class ParseFloats extends Stage {
         floats = Arrays.stream(value.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(",", "").split(" "))
             .map(v -> Float.parseFloat(v)).collect(Collectors.toList());
       } catch (Exception e) {
+        log.warn("String: {} cannot be parsed.", value, e);
         return null;
       }
       doc.removeField(this.field);
