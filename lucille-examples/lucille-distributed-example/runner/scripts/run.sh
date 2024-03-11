86117af690837dcea00bd09d4898d80d603c72d0
Akul Sethi <akul@kmwllc.com>
2024-03-11 16:05:42 -0400

    wip solr indexer does not work

--- lucille-examples/lucille-distributed-example/runner/scripts/run.sh
+++ lucille-examples/lucille-distributed-example/runner/scripts/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+mkdir output
+touch output/random
 java -Dconfig.file=/conf/main.conf -cp '/target/lib/*' com.kmwllc.lucille.core.Runner -usekafka
 # java -cp '/target/lib/*' junit.textui.TestRunner com.kmwllc.lucille.distributed.AfterTest
