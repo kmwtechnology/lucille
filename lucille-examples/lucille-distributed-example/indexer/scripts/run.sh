a1493fc1f9fa1fe9ea5eb7ea8674c503409cae8d
Akul Sethi <akul@kmwllc.com>
2024-03-13 10:17:46 -0400

    changed to csv indexer

--- lucille-examples/lucille-distributed-example/indexer/scripts/run.sh
+++ lucille-examples/lucille-distributed-example/indexer/scripts/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=quickstart&numShards=1&collection.configName=_default'
+mkdir output
 java -Dconfig.file=/conf/main.conf -cp '/target/lib/*' com.kmwllc.lucille.core.Indexer simple_pipeline
--- lucille-examples/lucille-distributed-example/runner/scripts/run.sh
+++ lucille-examples/lucille-distributed-example/runner/scripts/run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-mkdir output
-touch output/random
 java -Dconfig.file=/conf/main.conf -cp '/target/lib/*' com.kmwllc.lucille.core.Runner -usekafka
 # java -cp '/target/lib/*' junit.textui.TestRunner com.kmwllc.lucille.distributed.AfterTest
