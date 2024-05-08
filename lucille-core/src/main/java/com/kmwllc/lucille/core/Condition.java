0687238483bdbe90a13cee459cb451ecc1de80fe
Akul Sethi <akul@kmwllc.com>
2024-05-08 15:37:23 -0400

    fixed test

--- lucille-core/src/main/java/com/kmwllc/lucille/core/Condition.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/core/Condition.java
@@ -1,9 +1,11 @@
 package com.kmwllc.lucille.core;
 
 import com.typesafe.config.Config;
-
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -14,21 +16,26 @@ public class Condition implements Predicate<Document> {
   private final Operator operator;
 
   private enum Operator {
-    MUST, MUST_NOT;
+    MUST("must"), MUST_NOT("must_not");
 
-    public static Operator getOperator(String op) {
-      switch(op) {
-        case "must": return Operator.MUST;
-        case "must_not": return Operator.MUST_NOT;
-        default: throw new IllegalArgumentException(op + " is not a legal value for operator");
+    private final String val;
+
+    Operator(String val) {
+      this.val = val;
+    }
+
+    public static Operator get(String operator) {
+      Optional<Operator> temp = Arrays.stream(Operator.values()).filter(other -> other.val.equals(operator)).findFirst();
+      if (temp.isPresent()) {
+        return temp.get();
       }
+      throw new IllegalArgumentException("operator is invalid. It must take the value of `must` or `must_not`");
     }
   }
 
   public Condition(Config config) {
-    this(config.getStringList("fields"),
-        new HashSet<>(config.getStringList("values")),
-        config.hasPath("operator") ? Operator.getOperator(config.getString("operator")) : Operator.MUST);
+    this(config.getStringList("fields"), new HashSet<>(config.getStringList("values")),
+        config.hasPath("operator") ? Operator.get(config.getString("operator")) : Operator.MUST);
   }
 
   public Condition(List<String> fields, Set<String> values, Operator operator) {
