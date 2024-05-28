6866c8cb4947ad3e301b8636922553270898bb06
Akul Sethi <akul@kmwllc.com>
2024-05-28 10:10:24 -0400

    failsafe works

---
+++ lucille-core/src/test/java/com/kmwllc/lucille/core/MainIT.java
@@ -0,0 +1,12 @@
+package com.kmwllc.lucille.core;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MainIT {
+  
+  @Test 
+  public void fullIT() {
+    assertTrue(true);
+  }
+}
