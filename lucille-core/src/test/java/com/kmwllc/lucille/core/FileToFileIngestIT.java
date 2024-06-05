836e69de2d3a12c98b58c169e949f7b9e2156bef
Akul Sethi <akul@kmwllc.com>
2024-06-05 10:14:16 -0400

    removed file reader from test

--- lucille-core/src/test/java/com/kmwllc/lucille/core/FileToFileIngestIT.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/core/FileToFileIngestIT.java
@@ -1,31 +1,28 @@
 package com.kmwllc.lucille.core;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.util.List;
 import org.junit.Test;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 public class FileToFileIngestIT {
-  
-  @Test 
+
+  @Test
   public void fullIT() throws Exception {
     Config config = ConfigFactory.parseFile(new File("src/test/resources/FileToFileIngestIT/file-to-file-example.conf"));
     Runner.runInTestMode(config);
-
     File f = new File("output/dest.csv");
-    BufferedReader reader = new BufferedReader(new FileReader(f));
-    assertEquals("\"my_name\",\"my_country\",\"my_price\"", reader.readLine());
-    assertEquals("\"Carbonara\",\"Italy\",\"30\"", reader.readLine());
-    assertEquals("\"Pizza\",\"Italy\",\"10\"", reader.readLine());
-    assertEquals("\"Tofu Soup\",\"Korea\",\"12\"", reader.readLine());
-    assertEquals("\"my_name\",\"my_country\",\"my_price\"", reader.readLine());
-    assertEquals("\"Burger\",\"USA\",\"30\"", reader.readLine());
-    assertEquals("\"Salad\",\"USA\",\"10\"", reader.readLine());
-    assertEquals("\"Wine\",\"France\",\"12\"", reader.readLine());
-    reader.close();
+    List<String> lines = Files.readAllLines(f.toPath());
+    assertEquals("\"my_name\",\"my_country\",\"my_price\"", lines.get(0));
+    assertEquals("\"Carbonara\",\"Italy\",\"30\"", lines.get(1));
+    assertEquals("\"Pizza\",\"Italy\",\"10\"", lines.get(2));
+    assertEquals("\"Tofu Soup\",\"Korea\",\"12\"", lines.get(3));
+    assertEquals("\"my_name\",\"my_country\",\"my_price\"", lines.get(4));
+    assertEquals("\"Burger\",\"USA\",\"30\"", lines.get(5));
+    assertEquals("\"Salad\",\"USA\",\"10\"", lines.get(6));
+    assertEquals("\"Wine\",\"France\",\"12\"", lines.get(7));
   }
 }
