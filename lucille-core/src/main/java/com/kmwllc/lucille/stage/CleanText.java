29d6e04e7dbf5375eb87f56e130e3ceefa5fa5dc
Akul Sethi <akul@kmwllc.com>
2024-06-10 10:18:58 -0400

    added whitelist/blacklist

--- lucille-core/src/main/java/com/kmwllc/lucille/stage/CleanText.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/stage/CleanText.java
@@ -1,6 +1,7 @@
 package com.kmwllc.lucille.stage;
 
 import java.text.Normalizer;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import com.kmwllc.lucille.core.ConfigUtils;
@@ -39,15 +40,32 @@ import com.typesafe.config.Config;
 public class CleanText extends Stage {
 
   private final String source;
+  private final String destination;
   private final List<String> whitelist;
   private final List<String> blacklist;
 
 
   public CleanText(Config config) throws StageException {
-    super(config, new StageSpec().withRequiredProperties("source"));
+    super(config, new StageSpec().withRequiredProperties("source").withOptionalProperties("destination", "whitelist", "blacklist"));
     this.source = config.getString("source");
-    this.whitelist = ConfigUtils.getOrDefault(config, "whitelist", null);
-    this.blacklist = ConfigUtils.getOrDefault(config, "blacklist", null);
+    this.destination = ConfigUtils.getOrDefault(config, "destination", null);
+    this.whitelist = ConfigUtils.getOrDefault(config, "whitelist", Collections.emptyList());
+    this.blacklist = ConfigUtils.getOrDefault(config, "blacklist", Collections.emptyList());
+  }
+
+  @Override
+  public void start() throws StageException {
+    for (String s : whitelist) {
+      if (s.length() != 1) {
+        throw new StageException("whitelist must be list of single characters");
+      }
+    }
+
+    for (String s : blacklist) {
+      if (s.length() != 1) {
+        throw new StageException("blacklist must be list of single characters");
+      }
+    }
   }
 
   @Override
@@ -56,8 +74,24 @@ public class CleanText extends Stage {
       return null;
     }
 
-    doc.setField(source, Normalizer.normalize(doc.getString(source), Normalizer.Form.NFKD));
+    String output = Normalizer.normalize(doc.getString(source), Normalizer.Form.NFKD).replaceAll("\\p{M}", "");
+
+    for (String c : blacklist) {
+      output = output.replaceAll(c, "");
+    }
+ 
+    String temp = "";
+    for (int i = 0; i < output.length(); i++) {
+      if (whitelist.contains(String.valueOf(output.charAt(i)))) {
+        temp += output.charAt(i);
+      } 
+    }
+
+    if (destination == null) {
+       doc.setField(source, temp);
+    } else {
+       doc.setField(destination, temp);
+    }
     return null;
   }
-
 }
