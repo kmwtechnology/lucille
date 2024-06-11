1127bd309745a35dbc4c6a49abd85027e368ee99
Akul Sethi <akul@kmwllc.com>
2024-06-11 09:24:22 -0400

    added tests

---
+++ lucille-core/src/test/java/com/kmwllc/lucille/stage/CleanTextTest.java
@@ -0,0 +1,24 @@
+package com.kmwllc.lucille.stage;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.kmwllc.lucille.core.Document;
+import com.kmwllc.lucille.core.Stage;
+import com.kmwllc.lucille.core.StageException;
+
+public class CleanTextTest {
+
+  private StageFactory factory = StageFactory.of(CleanText.class);
+
+  @Test
+  public void testAccentsAndDiacratics() throws StageException {
+    Stage stage = factory.get("CleanTextTest/config.conf");
+    Document doc = Document.create("id");
+    doc.setField("foo", "āăąēîïĩíĝġńñšŝśûůŷ");
+
+    stage.processDocument(doc);
+
+    assertEquals(doc.getString("foo"), "āăąēîïĩíĝġńñšŝśûůŷ");
+    assertEquals(doc.getString("bar"), "aaaeiiiiggnnsssuuy");
+  }
+}
