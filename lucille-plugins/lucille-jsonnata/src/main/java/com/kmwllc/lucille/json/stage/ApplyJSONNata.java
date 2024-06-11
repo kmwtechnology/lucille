96a5e72dada2bee2070611cc36251fd7aca2fc64
Akul Sethi <akul@kmwllc.com>
2024-06-11 10:22:31 -0400

    basic stage done

--- lucille-plugins/lucille-jsonnata/src/main/java/com/kmwllc/lucille/json/stage/ApplyJSONNata.java
+++ lucille-plugins/lucille-jsonnata/src/main/java/com/kmwllc/lucille/json/stage/ApplyJSONNata.java
@@ -1,20 +1,27 @@
 package com.kmwllc.lucille.json.stage;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import java.io.IOException;
+import java.util.Iterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.api.jsonata4java.expressions.EvaluateException;
 import com.api.jsonata4java.expressions.Expressions;
+import com.api.jsonata4java.expressions.ParseException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.kmwllc.lucille.core.ConfigUtils;
 import com.kmwllc.lucille.core.Document;
 import com.kmwllc.lucille.core.Stage;
 import com.kmwllc.lucille.core.StageException;
 import com.typesafe.config.Config;
 
-import java.util.Iterator;
-
 public class ApplyJSONNata extends Stage {
+
+  private static final Logger log = LoggerFactory.getLogger(ApplyJSONNata.class);
 
   private final String source;
   private final String destination;
   private final String expression;
+  private Expressions parsed;
 
   public ApplyJSONNata(Config config) throws StageException {
     super(config, new StageSpec().withOptionalProperties("source", "destination").withRequiredProperties("expression"));
@@ -24,17 +31,36 @@ public class ApplyJSONNata extends Stage {
   }
 
   @Override
+  public void start() throws StageException {
+    try {
+      parsed = Expressions.parse(expression);
+    } catch (ParseException e) {
+      throw new StageException("Exception occured while parsing expression: " + e.getLocalizedMessage());
+    } catch (IOException e) {
+      throw new StageException("IO exception while parsing expression: " + e.getLocalizedMessage());
+    }
+  }
+
+  @Override
   public Iterator<Document> processDocument(Document doc) throws StageException {
     if (!doc.has(source)) {
       return null;
     }
-    
-    JsonNode output = Expressions.parse(expression).evaluate(doc.getJson(source));
-    
 
+    JsonNode output = null;
+    try {
+      output = parsed.evaluate(doc.getJson(source));
+    } catch (EvaluateException e) {
+      log.warn("Evaluation exception when applying JSONNata: ", e);
+      return null;
+    }
 
+    if (destination != null) {
+      doc.setField(destination, output);
+    } else {
+      doc.setField(source, output);
+    }
 
     return null;
   }
-
 }
