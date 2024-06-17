27d1db935fb650621631a73a4004460bf02c5a14
Akul Sethi <akul@kmwllc.com>
2024-06-17 13:22:30 -0400

    fix javadoc

--- lucille-core/src/main/java/com/kmwllc/lucille/stage/RemoveDiacritics.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/stage/RemoveDiacritics.java
@@ -8,38 +8,22 @@ import com.kmwllc.lucille.core.Stage;
 import com.kmwllc.lucille.core.StageException;
 import com.typesafe.config.Config;
 
-
 /**
- * Adds random data to a document field given parameters
+ * Removes diacritics and accents from String fields.
  * <br>
  * Config Parameters -
  * <br>
  * <p>
- * <b>inputDataPath</b> (String, Optional) : file path to a text file that stores datapoints to be randomly placed into field,
- *  defaults to numeric data based on range size (0 -&gt; rangeSize - 1)
+ * <b>source</b> (String) : Name of source field.
  * </p>
  * <p>
- * <b>fieldName</b> (String, Optional) : Field name of field where data is placed, defaults to "data"
- *  </p>
- * <p>
- * <b>rangeSize</b> (int, Optional) : size of the subset of datapoints to be grabbed either from
- *  given datapath or from random numbers
- * </p>
- *  <p>
- * <b>minNumOfTerms</b> (Integer, Optional) : minimum number of terms to be in the field, defaults to 1
- * </p>
- * <p>
- * <b>maxNumOfTerms</b> (Integer, Optional) : maximum number of terms to be in the field, defaults to 1
- * </p>
- * <p>
- * <b>isNested</b> (FieldType, Optional) : setting for structure of field, default or nested
+ * <b>destination</b> (String, Optional) : Name of field where transformed string is put. If not provided the string is mutated in place.
  * </p>
  */
 public class RemoveDiacritics extends Stage {
 
   private final String source;
   private final String destination;
-
 
   public RemoveDiacritics(Config config) throws StageException {
     super(config, new StageSpec().withRequiredProperties("source").withOptionalProperties("destination"));
