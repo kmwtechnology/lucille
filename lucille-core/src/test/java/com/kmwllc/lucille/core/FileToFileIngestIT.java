33b51f3eff17d2f9736dd01adb69d072eb642b79
Akul Sethi <akul@kmwllc.com>
2024-06-17 13:08:00 -0400

    clean up files

--- lucille-core/src/test/java/com/kmwllc/lucille/core/FileToFileIngestIT.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/core/FileToFileIngestIT.java
@@ -12,17 +12,22 @@ public class FileToFileIngestIT {
 
   @Test
   public void fullIT() throws Exception {
-    Config config = ConfigFactory.parseFile(new File("src/test/resources/FileToFileIngestIT/file-to-file-example.conf"));
-    Runner.runInTestMode(config);
-    File f = new File("output/dest.csv");
-    List<String> lines = Files.readAllLines(f.toPath());
-    assertEquals("\"my_name\",\"my_country\",\"my_price\"", lines.get(0));
-    assertEquals("\"Carbonara\",\"Italy\",\"30\"", lines.get(1));
-    assertEquals("\"Pizza\",\"Italy\",\"10\"", lines.get(2));
-    assertEquals("\"Tofu Soup\",\"Korea\",\"12\"", lines.get(3));
-    assertEquals("\"my_name\",\"my_country\",\"my_price\"", lines.get(4));
-    assertEquals("\"Burger\",\"USA\",\"30\"", lines.get(5));
-    assertEquals("\"Salad\",\"USA\",\"10\"", lines.get(6));
-    assertEquals("\"Wine\",\"France\",\"12\"", lines.get(7));
+    File f = null;
+    try {
+      Config config = ConfigFactory.parseFile(new File("src/test/resources/FileToFileIngestIT/file-to-file-example.conf"));
+      Runner.runInTestMode(config);
+      f = new File("output/dest.csv");
+      List<String> lines = Files.readAllLines(f.toPath());
+      assertEquals("\"my_name\",\"my_country\",\"my_price\"", lines.get(0));
+      assertEquals("\"Carbonara\",\"Italy\",\"30\"", lines.get(1));
+      assertEquals("\"Pizza\",\"Italy\",\"10\"", lines.get(2));
+      assertEquals("\"Tofu Soup\",\"Korea\",\"12\"", lines.get(3));
+      assertEquals("\"my_name\",\"my_country\",\"my_price\"", lines.get(4));
+      assertEquals("\"Burger\",\"USA\",\"30\"", lines.get(5));
+      assertEquals("\"Salad\",\"USA\",\"10\"", lines.get(6));
+      assertEquals("\"Wine\",\"France\",\"12\"", lines.get(7));
+    } finally {
+      f.delete();
+    }
   }
 }
