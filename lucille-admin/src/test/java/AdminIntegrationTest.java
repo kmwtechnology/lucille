26a035a6279e3d47721293d3290366ae33fc418b
ssolomon <spencer.solomon@kmwllc.com>
2024-07-16 13:42:38 -0400

    LC-469 cleanup the admin pom, endpoint and integration test

--- lucille-admin/src/test/java/AdminIntegrationTest.java
+++ lucille-admin/src/test/java/AdminIntegrationTest.java
@@ -1,28 +1,19 @@
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import com.kmwllc.lucille.AdminAPI;
 import com.kmwllc.lucille.LucilleAPIConfiguration;
-import com.kmwllc.lucille.core.RunnerManager;
-import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.core.Response;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
-import org.apache.kafka.clients.admin.Admin;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.mockito.Mock;
 
 public class AdminIntegrationTest {
 
   @ClassRule
-  public static final DropwizardAppRule<LucilleAPIConfiguration> RULE = new DropwizardAppRule<>(AdminAPI.class); //, ResourceHelpers.resourceFilePath("admin.yml"));
-
-  @Mock
-  RunnerManager rm = RunnerManager.getInstance();
+  public static final DropwizardAppRule<LucilleAPIConfiguration> RULE = new DropwizardAppRule<>(AdminAPI.class);
 
   private final Client client = RULE.client();
   private final String url = String.format("http://localhost:%d/", RULE.getLocalPort());
@@ -51,23 +42,4 @@ public class AdminIntegrationTest {
 
     assertEquals("{\"runId\":\"\",\"running\":false}", entity);
   }
-
-  @Test
-  public void testLucilleStart() throws Exception {
-    // TODO: Right now this test just ensures that running a POST to the /admin endpoint will call RunnerManager.run()
-    // TODO : We should somehow mock the RunnerManager so that we can confirm getRunStatus() works before, during and after a lucille run
-    when(rm.run()).thenReturn(true);
-
-    try (Response status = client.target(url + "admin").request().post(null)) {
-      status.bufferEntity();
-      byte[] bytes = ((ByteArrayInputStream) status.getEntity()).readAllBytes();
-      String entity = new String(bytes, StandardCharsets.UTF_8);
-
-      assertEquals("Lucille run has been triggered.", entity);
-    } catch (Exception e) {
-      throw new Exception(e);
-    }
-
-  }
-
 }
