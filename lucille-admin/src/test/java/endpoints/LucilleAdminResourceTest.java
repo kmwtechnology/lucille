759713a8881ef0947230bd56f8c7e6919caebecb
ssolomon <spencer.solomon@kmwllc.com>
2024-07-17 10:50:48 -0400

    LC-469 abstract the dropwizard version to the parent pom

--- lucille-admin/src/test/java/endpoints/LucilleAdminResourceTest.java
+++ lucille-admin/src/test/java/endpoints/LucilleAdminResourceTest.java
@@ -1,33 +1,34 @@
 package endpoints;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.kmwllc.lucille.core.RunnerManager;
 import com.kmwllc.lucille.endpoints.LucilleAdminResource;
 import jakarta.ws.rs.core.Response;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LucilleAdminResourceTest {
 
-  private static final LucilleAdminResource admin = new LucilleAdminResource();
+  private final RunnerManager runnerManager = RunnerManager.getInstance();
 
   @Test
   public void testGetRunStatus() {
+    LucilleAdminResource admin = new LucilleAdminResource(runnerManager);
     Response status = admin.getRunStatus();
 
-    assertEquals("{'isRunning': 'false', 'runId': ''}", status.getEntity().toString());
+    assertEquals("{'isRunning': 'true', 'runId': ''}", status.getEntity().toString());
   }
 
   @Test
   public void testRun() {
+    LucilleAdminResource admin = new LucilleAdminResource(runnerManager);
+
     // Ensure that a 200 OK is returned when the run is started
     Response status = Response.status(404).build();
 
