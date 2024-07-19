2bc0d1d321b5219b8fe692e07d4ecdc403c9d306
ssolomon <spencer.solomon@kmwllc.com>
2024-07-19 15:18:10 -0400

    LC-516 remove iterate docker development script

--- lucille-admin/iterate-docker.sh
+++
@@ -1,6 +0,0 @@
-export CONTAINERS=$(docker ps -a -q)
-docker stop ${CONTAINERS}
-docker rm ${CONTAINERS}
-mvn clean install -DskipTests
-docker build -t lucille-admin .
-docker run -p 8080:8080 lucille-admin
