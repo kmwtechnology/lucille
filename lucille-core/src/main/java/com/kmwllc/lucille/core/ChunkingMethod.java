4ec7b480eb3112cbed3258762313ed45205e52c8
Jozurf <tksjosephusa@gmail.com>
2024-08-05 14:03:55 -0400

    LC-467: implement chunk stage
    - added en-sent.bin (by opennlp), model which detect sentences, to lucille-core resources
    - added opennlp and jtokkit dependencies for detecting token length

---
+++ lucille-core/src/main/java/com/kmwllc/lucille/core/ChunkingMethod.java
@@ -0,0 +1,34 @@
+package com.kmwllc.lucille.core;
+
+import com.typesafe.config.Config;
+
+public enum ChunkingMethod {
+  FIXED("fixed"), CUSTOM("custom"), PARAGRAPH("paragraph"), ;
+
+  public static final String CONFIG_PATH = "chunking_method";
+  public static final ChunkingMethod DEFAULT = FIXED;
+
+  private String text;
+
+  ChunkingMethod(String text) {
+    this.text = text;
+  }
+
+  public static ChunkingMethod fromString(String modeStr) {
+    for (ChunkingMethod mode : ChunkingMethod.values()) {
+      if (modeStr.equals(mode.text)) {
+        return mode;
+      }
+    }
+
+    return DEFAULT;
+  }
+
+  public static ChunkingMethod fromConfig(Config config) {
+    if (config.hasPath(CONFIG_PATH)) {
+      return ChunkingMethod.fromString(config.getString(CONFIG_PATH));
+    }
+    return DEFAULT;
+  }
+
+}
