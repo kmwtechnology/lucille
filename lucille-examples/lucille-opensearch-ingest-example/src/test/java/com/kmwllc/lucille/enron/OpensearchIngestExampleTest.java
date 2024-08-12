a11a9ffe104922aab0effdeadd7beb0a28404ce9
Jozurf <tksjosephusa@gmail.com>
2024-08-12 15:41:00 -0400

    minor changes to test and config file

--- lucille-examples/lucille-opensearch-ingest-example/src/test/java/com/kmwllc/lucille/enron/OpensearchIngestExampleTest.java
+++ lucille-examples/lucille-opensearch-ingest-example/src/test/java/com/kmwllc/lucille/enron/OpensearchIngestExampleTest.java
@@ -19,23 +19,23 @@ public class OpensearchIngestExampleTest {
       File dir = new File(relativeDirPath).getAbsoluteFile();
 
       // check if the directory exists and is indeed a directory
-      if (dir.exists() && dir.isDirectory()) {
-        // list all conf files in the directory
-        File[] confFiles = dir.listFiles((d, name) -> name.endsWith(".conf"));
-
-        // loop through each conf file
-        for (File configFile : Objects.requireNonNull(confFiles)) {
-          // run the configFile and gather exceptions
-          Config config = ConfigFactory.parseFile(configFile);
-          Map<String, List<Exception>> exceptions = Runner.runInValidationMode(config);
-
-          // check that all validations have no exceptions
-          for (Map.Entry<String, List<Exception>> entry : exceptions.entrySet()) {
-            assertEquals(0, entry.getValue().size());
-          }
-        }
-      } else {
+      if (!dir.exists() || !dir.isDirectory()) {
         throw new RuntimeException("Directory " + relativeDirPath + " does not exist or is not a directory.");
+      }
+
+      // list all conf files in the directory
+      File[] confFiles = dir.listFiles((d, name) -> name.endsWith(".conf"));
+
+      // loop through each conf file
+      for (File configFile : Objects.requireNonNull(confFiles)) {
+        // run the configFile and gather exceptions
+        Config config = ConfigFactory.parseFile(configFile);
+        Map<String, List<Exception>> exceptions = Runner.runInValidationMode(config);
+
+        // check that all validations have no exceptions
+        for (Map.Entry<String, List<Exception>> entry : exceptions.entrySet()) {
+          assertEquals(0, entry.getValue().size());
+        }
       }
     }
 }
