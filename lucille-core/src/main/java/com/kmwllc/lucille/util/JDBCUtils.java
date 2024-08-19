04416e0e83ae08dbe34a35e75eeb55a9102ec7e8
Jozurf <tksjosephusa@gmail.com>
2024-08-19 13:47:15 -0400

    LC-536: added JDBCUtils for JDBC type handling

---
+++ lucille-core/src/main/java/com/kmwllc/lucille/util/JDBCUtils.java
@@ -0,0 +1,120 @@
+package com.kmwllc.lucille.util;
+
+import com.kmwllc.lucille.core.Document;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class JDBCUtils {
+
+  private static final Logger log = LogManager.getLogger(JDBCUtils.class);
+
+  public static void parseResultToDoc(Document doc, ResultSet rs, Map<String, Object> fieldMapping) throws SQLException {
+    ResultSetMetaData metadata = rs.getMetaData();
+    for (String key : fieldMapping.keySet()) {
+      String field = (String) fieldMapping.get(key);
+      int columnIndex = rs.findColumn(key);
+      int columnType = metadata.getColumnType(columnIndex);
+      typeHandling(doc, rs, field, columnIndex, columnType);
+    }
+  }
+
+  public static void parseResultToDoc(Document doc, ResultSet rs, String fieldName)
+      throws SQLException {
+    int columnIndex = rs.findColumn(fieldName);
+    parseResultToDoc(doc, rs, fieldName, columnIndex);
+  }
+
+  public static void parseResultToDoc(Document doc, ResultSet rs, String fieldName, int columnIndex)
+      throws SQLException {
+    ResultSetMetaData metadata = rs.getMetaData();
+    typeHandling(doc, rs, fieldName, columnIndex, metadata.getColumnType(columnIndex));
+  }
+
+  private static void typeHandling(Document doc, ResultSet rs, String fieldName, int columnIndex, int columnType)
+      throws SQLException {
+      switch (columnType) {
+        case Types.CLOB:
+        case Types.NCLOB:
+        case Types.VARCHAR:
+        case Types.CHAR:
+        case Types.LONGVARCHAR:
+        case Types.NCHAR:
+        case Types.NVARCHAR:
+        case Types.LONGNVARCHAR:
+          String str = rs.getString(columnIndex);
+          if (str != null) {
+            doc.setOrAdd(fieldName, str);
+          }
+          break;
+        case Types.TINYINT:
+        case Types.SMALLINT:
+        case Types.INTEGER:
+          int integer = rs.getInt(columnIndex);
+          // rs.getInt would return 0 if SQL value is null. This would to check that the field value is null
+          if (!rs.wasNull()) {
+            doc.setOrAdd(fieldName, integer);
+          }
+          break;
+        case Types.BIGINT:
+          long longVal = rs.getLong(columnIndex);
+          if (!rs.wasNull()) {
+            doc.setOrAdd(fieldName,longVal);
+          }
+          break;
+        case Types.DECIMAL:
+        case Types.NUMERIC:
+        case Types.DOUBLE:
+          double doubleVal = rs.getDouble(columnIndex);
+          // rs.getDouble would return 0 if SQL value is null. This would to check that the field value is null
+          if (!rs.wasNull()) {
+            doc.setOrAdd(fieldName, doubleVal);
+          }
+          break;
+        case Types.FLOAT:
+        case Types.REAL:
+          float floatVal = rs.getFloat(columnIndex);
+          // rs.getFloat would return 0 if SQL value is null. This would to check that the field value is null
+          if (!rs.wasNull()) {
+            doc.setOrAdd(fieldName, floatVal);
+          }
+          break;
+        case Types.BOOLEAN:
+        case Types.BIT:
+          boolean boolVal = rs.getBoolean(columnIndex);
+          // rs.getBoolean would return false if SQL value is null. This would to check that the field value is null
+          if (!rs.wasNull()) {
+            doc.setOrAdd(fieldName, boolVal);
+          }
+          break;
+        case Types.DATE:
+        case Types.TIMESTAMP:
+        case Types.TIMESTAMP_WITH_TIMEZONE:
+          // DATE: sets nanoseconds & time to 0, then checks either DB server local time (which might be from JVM), and converts to UTC
+          // -  JVM or DB server is UTC-4, will convert to UTC by adding 4 hours.
+          // e.g. 2023-01-01T00:00:00Z when UTC-4, will be stored as 2023-01-01T04:00:00Z in Document
+          // TIMESTAMP_WITH_TIMEZONE: converts it to UTC timing.
+          Timestamp timestamp = rs.getTimestamp(columnIndex);
+          if (timestamp != null) {
+            doc.setOrAdd(fieldName, timestamp.toInstant());
+          }
+          break;
+        case Types.BLOB:
+        case Types.BINARY:
+        case Types.VARBINARY:
+        case Types.LONGVARBINARY:
+          byte[] bytes = rs.getBytes(columnIndex);
+          if (bytes != null) {
+            doc.setOrAdd(fieldName, bytes);
+          }
+          break;
+        default:
+          log.warn("SQL Type {} not supported", columnType);
+      }
+  }
+}
--- lucille-core/src/test/java/com/kmwllc/lucille/stage/QueryDatabaseTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/stage/QueryDatabaseTest.java
@@ -1,4 +1,7 @@
 package com.kmwllc.lucille.stage;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -8,6 +11,12 @@ import com.kmwllc.lucille.connector.jdbc.DBTestHelper;
 import com.kmwllc.lucille.core.Document;
 import com.kmwllc.lucille.core.Stage;
 import com.kmwllc.lucille.core.StageException;
+import java.nio.charset.StandardCharsets;
+import java.util.TimeZone;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import java.util.Arrays;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.MockedStatic;
@@ -23,6 +32,18 @@ import static org.junit.Assert.assertThrows;
 public class QueryDatabaseTest {
 
   StageFactory factory = StageFactory.of(QueryDatabase.class);
+  private static TimeZone originalTimeZone;
+
+  @BeforeClass
+  static public void beforeClass() {
+    originalTimeZone = TimeZone.getDefault();
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
+
+  @AfterClass
+  static public void afterClass() {
+    TimeZone.setDefault(originalTimeZone);
+  }
 
   @Rule
   public final DBTestHelper dbHelper = new DBTestHelper("org.h2.Driver", "jdbc:h2:mem:test", "",
@@ -94,7 +115,6 @@ public class QueryDatabaseTest {
             "jdbcUser",
             "keyFields",
             "name",
-            "returnTypes",
             "jdbcPassword",
             "conditions",
             "class",
@@ -140,5 +160,155 @@ public class QueryDatabaseTest {
       Stage stage = factory.get("QueryDatabaseTest/animal.conf");
       assertThrows(StageException.class, () -> stage.stop());
     }
+  }
+
+  @Test
+  public void testTypes() throws StageException {
+    Stage stage = factory.get("QueryDatabaseTest/types.conf");
+
+    Document d1 = Document.create("id");
+    d1.setField("get_row", 1);
+    Document d2 = Document.create("id");
+    d2.setField("get_row", 2);
+    stage.processDocument(d1);
+    stage.processDocument(d2);
+    /*
+      types to test: varchar_col, char_col, longvarchar_col, nchar_col, nvarchar_col, longnvarchar_col, clob_col, nclob_col,
+      tinyint_col, smallint_col, integer_col, bigint_col, double_col, decimal_col, numeric_col, float_col, real_col,
+      boolean_col, bit_col, date_col, timestamp_col, timestamp_w_timezone_col, blob_col, binary_col, varbinary_col, longvarbinary_col,
+      nullable_int, nullable_varchar, nullable_date
+    */
+
+    // testing out d1
+    assertEquals("Test VARCHAR", d1.getString("varchar_col"));
+    // calling trim as char_col is storing fixed number of characters
+    assertEquals("CHAR Test", d1.getString("char_col").trim());
+    assertEquals("Long VARCHAR test data", d1.getString("longvarchar_col"));
+    // calling trim as nchar_col is storing fixed number of characters
+    assertEquals("\uD83D\uDE00", d1.getString("nchar_col").trim());
+    assertEquals("こんにちは、世界！", d1.getString("nvarchar_col"));
+    assertEquals("こんにちは、世界！長いテキストのテストです。", d1.getString("longnvarchar_col"));
+    assertEquals("test clob", d1.getString("clob_col"));
+    assertEquals("test nclob", d1.getString("nclob_col"));
+    assertEquals(Integer.valueOf(127), d1.getInt("tinyint_col"));
+    assertEquals(Integer.valueOf(32767), d1.getInt("smallint_col"));
+    assertEquals(Integer.valueOf(2147483647), d1.getInt("integer_col"));
+    assertEquals(Long.valueOf("9223372036854775807"), d1.getLong("bigint_col"));
+    assertEquals(Double.valueOf(3.14159265359), d1.getDouble("double_col"));
+    assertEquals(Double.valueOf(9877.0), d1.getDouble("decimal_col"));
+    assertEquals(Double.valueOf(1.0), d1.getDouble("numeric_col"));
+    assertEquals(Float.valueOf("2.71828"), d1.getFloat("float_col"));
+    assertEquals(Float.valueOf("1.414214"), d1.getFloat("real_col"));
+    assertEquals(true, d1.getBoolean("boolean_col"));
+    assertEquals(true, d1.getBoolean("bit_col"));
+    // note that we have set the TimeZone in this test class to be UTC
+    // if it is not set, then the values would change according to the JVM Timezone at runtime
+    assertEquals("2024-07-30T00:00:00Z", d1.getInstant("date_col").toString());
+    assertEquals("1970-01-01T00:00:01Z", d1.getInstant("timestamp_col").toString());
+    assertEquals("1969-12-31T19:00:01Z", d1.getInstant("timestamp_w_timezone_col").toString());
+    assertFalse(d1.has("nullable_int"));
+    assertNull(d1.getString("nullable_varchar"));
+    // date would not get added to docs if null
+    assertFalse(d1.has("nullable_date"));
+    // converting blob to Expected String
+    assertEquals("This is a test blob.", new String(d1.getBytes("blob_col"), StandardCharsets.UTF_8));
+    // converting binaryColBytes from array of 100 to 4 and comparing
+    byte[] binaryColBytes = d1.getBytes("binary_col");
+    byte[] expectedBytes = new byte[] {(byte)0xBE, (byte)0xEF, (byte)0xBE, (byte)0xEF};
+    assertArrayEquals(expectedBytes, Arrays.copyOf(binaryColBytes, 4));
+
+    byte[] varbinaryColBytes = d1.getBytes("varbinary_col");
+    byte[] expectedVarbinaryBytes = new byte[] {
+        0x01, 0x23, 0x45, 0x67, (byte)0x89, (byte)0xAB, (byte)0xCD, (byte)0xEF
+    };
+    assertArrayEquals(expectedVarbinaryBytes, varbinaryColBytes);
+
+    byte[] longVarbinaryColBytes = d1.getBytes("longvarbinary_col");
+    byte[] expectedLongVarbinaryBytes = new byte[] {
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, (byte)0x88, (byte)0x99, 0x00
+    };
+    assertArrayEquals(expectedLongVarbinaryBytes, longVarbinaryColBytes);
+
+    // testing out d2
+    assertEquals("Another", d2.getString("varchar_col"));
+    // calling trim as char_col is storing fixed number of characters
+    assertEquals("Test", d2.getString("char_col").trim());
+    assertEquals("More long text here", d2.getString("longvarchar_col"));
+    // calling trim as nchar_col is storing fixed number of characters
+    assertEquals("\uD83D\uDE00", d2.getString("nchar_col").trim());
+    assertEquals("안녕하세요, 세계!", d2.getString("nvarchar_col"));
+    assertEquals("안녕하세요, 세계! 긴 텍스트 테스트입니다.", d2.getString("longnvarchar_col"));
+    assertEquals("test clob", d2.getString("clob_col"));
+    assertEquals("test nclob", d2.getString("nclob_col"));
+    assertEquals(Integer.valueOf(-128), d2.getInt("tinyint_col"));
+    assertEquals(Integer.valueOf(-32768), d2.getInt("smallint_col"));
+    assertEquals(Integer.valueOf(-2147483648), d2.getInt("integer_col"));
+    assertEquals(Long.valueOf("-9223372036854775808"), d2.getLong("bigint_col"));
+    assertEquals(Double.valueOf(1.41421356237), d2.getDouble("double_col"));
+    assertEquals(Double.valueOf(500.0), d2.getDouble("decimal_col"));
+    assertEquals(Double.valueOf(100000.0), d2.getDouble("numeric_col"));
+    assertEquals(Float.valueOf("1.61803"), d2.getFloat("float_col"));
+    assertEquals(Float.valueOf("3.141592"), d2.getFloat("real_col"));
+    assertEquals(false, d2.getBoolean("boolean_col"));
+    assertEquals(false, d2.getBoolean("bit_col"));
+    // note that we have set the TimeZone in this test class to be UTC
+    // if it is not set, then the values would change according to the JVM Timezone at runtime
+    assertEquals("2023-01-01T00:00:00Z", d2.getInstant("date_col").toString());
+    assertEquals("2038-01-19T03:14:07Z", d2.getInstant("timestamp_col").toString());
+    assertEquals("2038-01-19T08:14:07Z", d2.getInstant("timestamp_w_timezone_col").toString());
+    assertFalse(d2.has("nullable_int"));
+    assertNull(d2.getString("nullable_varchar"));
+    // date would not get added to docs if null
+    assertFalse(d2.has("nullable_date"));
+    // converting blob to Expected String
+    assertEquals("This is a test blob2.", new String(d2.getBytes("blob_col"), StandardCharsets.UTF_8));
+    // converting binaryColBytes from array of 100 to 4 and comparing
+    binaryColBytes = d2.getBytes("binary_col");
+    expectedBytes = new byte[] {(byte)0xBE, (byte)0xEF, (byte)0xBE, (byte)0xEF};
+    assertArrayEquals(expectedBytes, Arrays.copyOf(binaryColBytes, 4));
+
+    varbinaryColBytes = d2.getBytes("varbinary_col");
+    expectedVarbinaryBytes = new byte[] {
+        (byte)0xFE, (byte)0xDC, (byte)0xBA, (byte)0x98,
+        0x76, 0x54, 0x32, 0x10
+    };
+    assertArrayEquals(expectedVarbinaryBytes, varbinaryColBytes);
+
+    longVarbinaryColBytes = d2.getBytes("longvarbinary_col");
+    expectedLongVarbinaryBytes = new byte[] {
+        (byte)0xAA, (byte)0xBB, (byte)0xCC, (byte)0xDD, (byte)0xEE, (byte)0xFF,
+        0x00, 0x11, 0x22, 0x33
+    };
+    assertArrayEquals(expectedLongVarbinaryBytes, longVarbinaryColBytes);
+  }
+
+  @Test
+  public void testUnsupportedTypeTime() throws StageException {
+    Stage stage = factory.get("QueryDatabaseTest/time_type.conf");
+
+    Document d = Document.create("id");
+    d.setField("get_row", 1);
+    // JDBCUtils should log warning
+    assertEquals("no Error found", getUnsupportedMsg(stage, d));;
+  }
+
+  @Test
+  public void testUnsupportedTypeTimeWTimezone() throws StageException {
+    Stage stage = factory.get("QueryDatabaseTest/time_w_timezone_type.conf");
+
+    Document d = Document.create("id");
+    d.setField("get_row", 1);
+    // JDBCUtils should log warning
+    assertEquals("no Error found", getUnsupportedMsg(stage, d));
+  }
+
+  private String getUnsupportedMsg(Stage stage, Document d) {
+    try {
+      stage.processDocument(d);
+      System.out.println(d.asMap());
+    } catch (StageException e) {
+      return e.getMessage();
+    }
+    return "no Error found";
   }
 }
