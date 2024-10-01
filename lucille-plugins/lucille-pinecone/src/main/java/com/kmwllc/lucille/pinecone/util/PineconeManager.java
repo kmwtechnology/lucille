bbd7647c45831ac631836641a32d9caf18d298ad
Jozurf <tksjosephusa@gmail.com>
2024-10-01 14:18:53 -0400

    LC-550: removed unused imports and fields in PineconeManager

--- lucille-plugins/lucille-pinecone/src/main/java/com/kmwllc/lucille/pinecone/util/PineconeManager.java
+++ lucille-plugins/lucille-pinecone/src/main/java/com/kmwllc/lucille/pinecone/util/PineconeManager.java
@@ -1,14 +1,10 @@
 package com.kmwllc.lucille.pinecone.util;
 
 import io.pinecone.clients.Pinecone;
-import java.lang.invoke.MethodHandles;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class PineconeManager {
-  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   private static final String DEFAULT_NAMESPACE = "default";
   private static final Map<String, Pinecone> clients = new ConcurrentHashMap<>();
 
