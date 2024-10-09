07da2d0c0a6294941b2434def1580aaed90a576a
Jozurf <tksjosephusa@gmail.com>
2024-10-09 11:15:55 -0400

    LC-439: updated error message

--- lucille-core/src/main/java/com/kmwllc/lucille/connector/jdbc/DatabaseConnector.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/connector/jdbc/DatabaseConnector.java
@@ -261,7 +261,7 @@ public class DatabaseConnector extends AbstractConnector {
       } catch (NullPointerException e) {
         throw new ConnectorException("Either otherJoinId or joinId is null.", e);
       } catch (ClassCastException e) {
-        throw new ConnectorException("Either otherJoinId or JoinId class type prevents them from being compared.", e);
+        throw new ConnectorException("Either otherJoinId or joinId type prevents them from being compared.", e);
       }
       if (comparison < 0) {
         // advance until we get to the id on the right side that we want.
