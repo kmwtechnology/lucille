b01fd126eb360c33a9b8f198a70c5d32cce48359
Jozurf <tksjosephusa@gmail.com>
2024-10-21 14:48:54 -0400

    LC-334: added TestConfUtils

---
+++ lucille-core/src/main/java/com/kmwllc/lucille/util/TestConfUtils.java
@@ -0,0 +1,51 @@
+package com.kmwllc.lucille.util;
+
+import com.kmwllc.lucille.core.Runner;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+/**
+ * This class is only meant for testing configs in the Lucille example module.
+ */
+
+public class TestConfUtils {
+
+  public static void validateConfigs(List<String> configsToIgnore) throws Exception {
+    // get path to conf directory
+    Path currentDir = Paths.get("").toAbsolutePath();
+    Path confDir = currentDir.resolve("conf");
+
+    try (Stream<Path> confStream = Files.list(confDir)) {
+      // retrieve all conf files in conf folder
+      List<Path> confPaths = confStream.filter(p -> p.toString().endsWith(".conf")).toList();
+
+      // validate each of the conf files
+      for (Path configPath : confPaths) {
+        if (configsToIgnore.contains(configPath.getFileName().toString())) {
+          continue;
+        }
+        // run the config and gather exceptions
+        Config config = ConfigFactory.parseFile(configPath.toFile());
+        Map<String, List<Exception>> exceptions = Runner.runInValidationMode(config);
+
+        for (Map.Entry<String, List<Exception>> entry : exceptions.entrySet()) {
+          if (!entry.getValue().isEmpty()) {
+            throw new Exception("configuration validation failed for " + entry.getKey() + ": " + entry.getValue());
+          }
+        }
+      }
+    }
+  }
+
+  public static void validateConfigs() throws Exception {
+    validateConfigs(new ArrayList<>());
+  }
+
+}
--- lucille-examples/lucille-simple-csv-solr-example/src/test/java/com/kmwllc/lucille/simpleCsvSolr/TestConfigs.java
+++ lucille-examples/lucille-simple-csv-solr-example/src/test/java/com/kmwllc/lucille/simpleCsvSolr/TestConfigs.java
@@ -1,57 +1,12 @@
 package com.kmwllc.lucille.simpleCsvSolr;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import com.kmwllc.lucille.core.Runner;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
-import org.junit.Before;
+import com.kmwllc.lucille.util.TestConfUtils;
 import org.junit.Test;
 
 public class TestConfigs {
-  List<String> configsToIgnore;
-
-  /**
-   * add to list of configsToIgnore if they are not meant to be validated.
-   */
-  @Before
-  public void setUp() {
-    configsToIgnore = new ArrayList<>();
-  }
 
   @Test
   public void testConf() throws Exception {
-    // get path to conf directory
-    Path currentDir = Paths.get("").toAbsolutePath();
-    Path confDir = currentDir.resolve("conf");
-    assertNotNull(confDir);
-
-    try (Stream<Path> confStream = Files.list(confDir)) {
-      // retrieve all conf files in conf folder
-      List<Path> confPaths = confStream.filter(p -> p.toString().endsWith(".conf")).toList();
-
-      // validate each of the conf files
-      for (Path configPath : confPaths) {
-        if (configsToIgnore.contains(configPath.getFileName().toString())) {
-          continue;
-        }
-        // run the config and gather exceptions
-        Config config = ConfigFactory.parseFile(configPath.toFile());
-        Map<String, List<Exception>> exceptions = Runner.runInValidationMode(config);
-
-        // check that all validations have no exceptions
-        for (Map.Entry<String, List<Exception>> entry : exceptions.entrySet()) {
-          assertEquals(0, entry.getValue().size());
-        }
-      }
-    }
+    TestConfUtils.validateConfigs();
   }
 }
