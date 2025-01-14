f3ae016f0253e9fad96aef64fd4db175b0608301
supertick <supertick@gmail.com>
2025-01-14 13:08:01 -0800

    updates for pr

--- lucille-examples/lucille-s3-ingest-example/scripts/run_ingest.sh
+++ lucille-examples/lucille-s3-ingest-example/scripts/run_ingest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# run this script from top level lucille-simple-csv-solr-example directory via ./scripts/run_ingest.sh
+# run this script from root of this example e.g. ./scripts/run_ingest.sh
 java -Dconfig.file=conf/s3-opensearch.conf -cp 'target/lib/*' com.kmwllc.lucille.core.Runner
