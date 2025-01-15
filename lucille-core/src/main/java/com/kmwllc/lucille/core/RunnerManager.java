379b81fdd6d978ae0b974ea660ed8cb57f9086e3
supertick <supertick@gmail.com>
2025-01-14 22:15:01 -0800

    fixed

--- lucille-core/src/main/java/com/kmwllc/lucille/core/RunnerManager.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/core/RunnerManager.java
@@ -149,7 +149,7 @@ public class RunnerManager {
 
         log.info(config.entrySet().toString());
 
-        runDetails.setRunResult(Runner.runWithResultLog(config, runDetails.getRunType()));
+        runDetails.setRunResult(Runner.runWithResultLog(config, runDetails.getRunType(), runId));
       } catch (Exception e) {
         log.error("Failed to run lucille with ID '{}' via the Runner Manager.", runId, e);
         runDetails.setError(e);
