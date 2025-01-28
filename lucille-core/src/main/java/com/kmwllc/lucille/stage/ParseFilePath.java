f633a04bb42b2750f567fb942fb5f7d6070b3d60
Kevin Watters <kwatters@kmwllc.com>
2025-01-28 08:50:56 -0500

    LC-606: ParseFilePath Stage + Unit Tests (#238)
    * adding a stage that can parse a file path and pull out various components into fields on the document.
    
    * cleanup the unit test a bit add some javadocs.
    
    * ..
    
    * ...
    
    * fixing up more operating system dependent failures (windows vs unix)
    
    * LC-608: Cleanup + refactoring + more unit tests.
    
    * LC-606: More reliance on FilenameUtils, enforcing valid separators, enhanced tests
    
    * LC-606: Removed use of File
    
    * LC-606: Removed unused test conf, updated comments, normalization of paths, updated tests
    
    * LC-606: Updated JavaDoc on FileSep
    
    * LC-606: Updated comment + unit tests emphasize fileSep behavior
    
    * LC-606: Helpful illegal argument exception message.
    
    ---------
    
    Co-authored-by: Jake Squatrito <jake.squatrito@icloud.com>

---
+++ lucille-core/src/main/java/com/kmwllc/lucille/stage/ParseFilePath.java
@@ -0,0 +1,93 @@
+package com.kmwllc.lucille.stage;
+
+import com.kmwllc.lucille.core.ConfigUtils;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.kmwllc.lucille.core.Document;
+import com.kmwllc.lucille.core.Stage;
+import com.kmwllc.lucille.core.StageException;
+import com.typesafe.config.Config;
+
+/**
+ * This stage will take a field that contains a file path (like  c:\directory\filename.txt) and parse out information about the
+ * path:
+ *   <br> - filename: The name of the file.
+ *   <br> - folder: The parent of the file.
+ *   <br> - path: The path to the file. (Normalized)
+ *   <br> - file_extension: The extension of the file.
+ *
+ * <br> <br> <b>Config Parameters:</b>
+ *
+ *   <br> - filePathField (String, Optional) - The field name that contains the file path. Defaults to "file_path".
+ *   <br> - fileSep (String, Optional) - The separator to use in the normalized path output from this stage.
+ *   Defaults to the operating system's separator. <b>Must</b> be either "/" or "\". You should specify the separator if you are
+ *   running Lucille on Windows but working with Unix file paths (or vice versa) and you want the original separators to be
+ *   preserved. Note that this stage will parse Windows and Unix paths regardless of fileSep / operating system - this property
+ *   only controls output behavior.
+ *   <br> - uppercaseExtension (Boolean, Optional) - If true, the extracted file extension will be in all uppercase letters. Defaults
+ *   to true.
+ *   <br> - includeHierarchy (Boolean, Optional) - If true, a field ("file_paths") will be populated with all of the normalized
+ *   subpaths leading up to the full file path (to aid in building a hierarchical aggregator/facet for a search engine).
+ *   
+ */
+public class ParseFilePath extends Stage {
+
+  private final String filePathField;
+  private final String fileSep;
+  private final boolean uppercaseExtension;
+  private final boolean includeHierarchy;
+
+  private static final Logger log = LogManager.getLogger(ParseFilePath.class);
+  
+  public ParseFilePath(Config config) {
+    super(config, new StageSpec().withOptionalProperties("filePathField", "fileSep", "uppercaseExtension", "includeHierarchy"));
+    this.filePathField = ConfigUtils.getOrDefault(config, "filePathField", "file_path");
+    this.fileSep = ConfigUtils.getOrDefault(config, "fileSep", File.separator);
+
+    if (!fileSep.equals("/") && !fileSep.equals("\\")) {
+      throw new IllegalArgumentException("ParseFilePath must have fileSep set to either / or \\. Remember to escape \\ in your .conf.");
+    }
+
+    this.uppercaseExtension = ConfigUtils.getOrDefault(config, "uppercaseExtension", true);
+    this.includeHierarchy = ConfigUtils.getOrDefault(config, "includeHierarchy", true);
+  }
+
+  @Override
+  public Iterator<Document> processDocument(Document doc) throws StageException {
+    if (!doc.has(filePathField)) {
+      return null;
+    }
+
+    String filePath = doc.getString(filePathField);
+    boolean useUnix = fileSep.equals("/");
+    filePath = FilenameUtils.normalizeNoEndSeparator(filePath, useUnix);
+
+    doc.addToField("filename", FilenameUtils.getName(filePath));
+    doc.addToField("folder", FilenameUtils.getFullPathNoEndSeparator(filePath));
+    doc.addToField("path", filePath);
+
+    if (uppercaseExtension) {
+      doc.addToField("file_extension", FilenameUtils.getExtension(filePath).toUpperCase());
+    } else {
+      doc.addToField("file_extension", FilenameUtils.getExtension(filePath));
+    }
+
+    if (includeHierarchy) {
+      String[] paths = StringUtils.split(FilenameUtils.normalizeNoEndSeparator(filePath, useUnix), fileSep);
+      for (int i = 1; i < paths.length; i++) {
+        String subPath = StringUtils.join(Arrays.copyOfRange(paths, 0, i), fileSep);
+        doc.addToField("file_paths", subPath);
+      }
+    }
+
+    return null;
+  }
+}
+
