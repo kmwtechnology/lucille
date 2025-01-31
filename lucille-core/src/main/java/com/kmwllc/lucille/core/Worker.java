c255972189e69a0bb7a4cf75d8a4d93c737d422a
Jake Squatrito <jake.squatrito@icloud.com>
2025-01-31 14:43:25 -0500

    LC-609: DOC ID in worker.

--- lucille-core/src/main/java/com/kmwllc/lucille/core/Worker.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/core/Worker.java
@@ -75,68 +75,69 @@ class Worker implements Runnable {
         continue;
       }
 
-      if (trackRetries && counter.add(doc)) {
-        try {
-          log.info("Retry count exceeded for document " + doc.getId() + "; Sending to failure topic");
-          messenger.sendFailed(doc);
-        } catch (Exception e) {
-          log.error("Failed to send doc to failure topic: " + doc.getId(), e);
+      try (MDC.MDCCloseable docIdMDC = MDC.putCloseable(ID_FIELD, doc.getId())) {
+        if (trackRetries && counter.add(doc)) {
+          try {
+            log.info("Retry count exceeded for document " + doc.getId() + "; Sending to failure topic");
+            messenger.sendFailed(doc);
+          } catch (Exception e) {
+            log.error("Failed to send doc to failure topic: " + doc.getId(), e);
+          }
+
+          try {
+            messenger.sendEvent(doc, "SENT_TO_DLQ", Event.Type.FAIL);
+          } catch (Exception e) {
+            log.error("Failed to send completion event for: " + doc.getId(), e);
+          }
+
+          commitOffsetsAndRemoveCounter(doc);
+          continue;
         }
 
         try {
-          messenger.sendEvent(doc, "SENT_TO_DLQ", Event.Type.FAIL);
+          Timer.Context context = timer.time();
+          Iterator<Document> results = pipeline.processDocument(doc);
+
+          while (results.hasNext()) {
+
+            Document result = results.next();
+
+            // if we're looking at a child document, send a CREATE events for it;
+            // a document is a child if it has a different ID from the input document;
+            // Note: we want to make sure that the Publisher is notified of any generated children
+            // BEFORE the input/parent document is completed. This prevents a situation where the Runner
+            // assumes the run is complete because the parent is complete and the Publisher didn't know
+            // about the children. This code assumes the pipeline emits children before parents.
+            if (!doc.getId().equals(result.getId())) {
+              messenger.sendEvent(result, null, Event.Type.CREATE);
+            }
+
+            if (result.isDropped()) {
+              messenger.sendEvent(result, null, Event.Type.DROP);
+            } else {
+              // send the completed document to the queue for indexing
+              messenger.sendForIndexing(result);
+            }
+          }
+
+          context.stop();
         } catch (Exception e) {
-          log.error("Failed to send completion event for: " + doc.getId(), e);
+          log.error("Error processing document: " + doc.getId(), e);
+          try {
+            messenger.sendEvent(doc, null, Event.Type.FAIL);
+          } catch (Exception e2) {
+            log.error("Error sending failure event for document: " + doc.getId(), e2);
+          }
+
+          commitOffsetsAndRemoveCounter(doc);
+
+          continue;
         }
 
         commitOffsetsAndRemoveCounter(doc);
-        continue;
       }
-
-      try {
-        Timer.Context context = timer.time();
-        Iterator<Document> results = pipeline.processDocument(doc);
-
-        while (results.hasNext()) {
-
-          Document result = results.next();
-
-          // if we're looking at a child document, send a CREATE events for it;
-          // a document is a child if it has a different ID from the input document;
-          // Note: we want to make sure that the Publisher is notified of any generated children
-          // BEFORE the input/parent document is completed. This prevents a situation where the Runner
-          // assumes the run is complete because the parent is complete and the Publisher didn't know
-          // about the children. This code assumes the pipeline emits children before parents.
-          if (!doc.getId().equals(result.getId())) {
-            messenger.sendEvent(result, null, Event.Type.CREATE);
-          }
-
-          if (result.isDropped()) {
-            messenger.sendEvent(result, null, Event.Type.DROP);
-          } else {
-            // send the completed document to the queue for indexing
-            messenger.sendForIndexing(result);
-          }
-        }
-
-        context.stop();
-      } catch (Exception e) {
-        log.error("Error processing document: " + doc.getId(), e);
-        try {
-          messenger.sendEvent(doc, null, Event.Type.FAIL);
-        } catch (Exception e2) {
-          log.error("Error sending failure event for document: " + doc.getId(), e2);
-        }
-
-        commitOffsetsAndRemoveCounter(doc);
-
-        continue;
-      }
-
-      commitOffsetsAndRemoveCounter(doc);
     }
 
-    MDC.remove(ID_FIELD);
     // commit any remaining offsets before termination
     commitOffsetsAndRemoveCounter(null);
 
