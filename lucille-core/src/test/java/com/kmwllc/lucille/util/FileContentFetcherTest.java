8527e0302c31edefaa84a3bb4801cba8a9254ff3
Jake Squatrito <jake.squatrito@icloud.com>
2025-02-25 11:38:08 -0500

    LC-534: Improved FileContentFetcherTest to use multiple cloud storage clients

--- lucille-core/src/test/java/com/kmwllc/lucille/util/FileContentFetcherTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/util/FileContentFetcherTest.java
@@ -1,5 +1,6 @@
 package com.kmwllc.lucille.util;
 
+import static com.kmwllc.lucille.connector.FileConnector.GOOGLE_SERVICE_KEY;
 import static com.kmwllc.lucille.connector.FileConnector.S3_ACCESS_KEY_ID;
 import static com.kmwllc.lucille.connector.FileConnector.S3_REGION;
 import static com.kmwllc.lucille.connector.FileConnector.S3_SECRET_ACCESS_KEY;
@@ -11,6 +12,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.kmwllc.lucille.connector.storageclient.GoogleStorageClient;
 import com.kmwllc.lucille.connector.storageclient.S3StorageClient;
 import com.kmwllc.lucille.core.ConnectorException;
 import com.typesafe.config.Config;
@@ -55,19 +57,25 @@ public class FileContentFetcherTest {
 
   @Test
   public void testMultipleStorageClients() throws Exception {
-    // TODO: Add another cloud storage client that'll get ubilt here
     Config cloudOptions = ConfigFactory.parseMap(Map.of(
         S3_REGION, "us-east-1",
         S3_ACCESS_KEY_ID, "accessKey",
-        S3_SECRET_ACCESS_KEY, "secretKey"));
+        S3_SECRET_ACCESS_KEY, "secretKey",
+        GOOGLE_SERVICE_KEY, "serviceKey"));
 
     URI s3URI = URI.create("s3://bucket/hello.txt");
+    URI googleURI = URI.create("gs://bucket/hello.txt");
 
     // Mocking construction - just want to make sure that the fetcher is appropriately deferring to each storage client
     // as appropriate based on the strings it is supplied.
-    try (MockedConstruction<S3StorageClient> mockedConstruction = mockConstruction(S3StorageClient.class, (mock, context) -> {
-      when(mock.getFileContentStream(s3URI)).thenReturn(new ByteArrayInputStream("Hello there - S3.".getBytes()));
-    })) {
+    try (
+        MockedConstruction<S3StorageClient> mockedConstruction = mockConstruction(S3StorageClient.class, (mock, context) -> {
+          when(mock.getFileContentStream(s3URI)).thenReturn(new ByteArrayInputStream("Hello there - S3.".getBytes()));
+        });
+        MockedConstruction<GoogleStorageClient> mockedGoogle = mockConstruction(GoogleStorageClient.class, (mock, context) -> {
+          when(mock.getFileContentStream(googleURI)).thenReturn(new ByteArrayInputStream("Hello there - Google.".getBytes()));
+        });
+    ) {
       FileContentFetcher fetcher = new FileContentFetcher(cloudOptions);
 
       URI localPathURI = Paths.get("src/test/resources/FileContentFetcherTest/hello.txt").toUri();
@@ -77,7 +85,10 @@ public class FileContentFetcherTest {
       InputStream s3MockInputStream = fetcher.getInputStream(s3URI.toString());
       assertEquals("Hello there - S3.", new String(s3MockInputStream.readAllBytes()));
 
-      assertThrows(IOException.class, () -> fetcher.getInputStream("gs://bucket/hello.txt"));
+      InputStream googleMockInputStream = fetcher.getInputStream(googleURI.toString());
+      assertEquals("Hello there - Google.", new String(googleMockInputStream.readAllBytes()));
+
+      assertThrows(IOException.class, () -> fetcher.getInputStream("https://storagename.blob.core.windows.net/bucket/hello.txt"));
     }
   }
 
