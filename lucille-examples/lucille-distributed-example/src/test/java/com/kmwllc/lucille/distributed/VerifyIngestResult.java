aabbb4fa7ca86b81f140526b81b8dbb8e55adb52
Jake Squatrito <jake.squatrito@icloud.com>
2025-03-05 09:38:17 -0500

    LC-643: Sort the results. Update the test with the new, enforced order.

--- lucille-examples/lucille-distributed-example/src/test/java/com/kmwllc/lucille/distributed/VerifyIngestResult.java
+++ lucille-examples/lucille-distributed-example/src/test/java/com/kmwllc/lucille/distributed/VerifyIngestResult.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import org.junit.Test;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -27,7 +28,27 @@ public class VerifyIngestResult {
       docs.add(json.get("response").get("docs").get(i));
     }
 
-    JsonNode csvDoc1 = docs.get(0);
+    docs.sort(Comparator.comparing(o -> o.get("id").asText()));
+
+    JsonNode jsonDoc1 = docs.get(0);
+    assertEquals("3", jsonDoc1.get("id").asText());
+    assertEquals("USA", jsonDoc1.get("my_country").get(0).asText());
+    assertEquals("Burger", jsonDoc1.get("my_name").get(0).asText());
+    assertEquals(30, jsonDoc1.get("my_price").get(0).asInt());
+
+    JsonNode jsonDoc2 = docs.get(1);
+    assertEquals("4", jsonDoc2.get("id").asText());
+    assertEquals("USA", jsonDoc2.get("my_country").get(0).asText());
+    assertEquals("Salad", jsonDoc2.get("my_name").get(0).asText());
+    assertEquals(10, jsonDoc2.get("my_price").get(0).asInt());
+
+    JsonNode jsonDoc3 = docs.get(2);
+    assertEquals("5", jsonDoc3.get("id").asText());
+    assertEquals("France", jsonDoc3.get("my_country").get(0).asText());
+    assertEquals("Wine", jsonDoc3.get("my_name").get(0).asText());
+    assertEquals(12, jsonDoc3.get("my_price").get(0).asInt());
+
+    JsonNode csvDoc1 = docs.get(3);
     assertEquals("source.csv-1", csvDoc1.get("id").asText());
     assertEquals("/conf/source.csv", csvDoc1.get("source").get(0).asText());
     assertEquals("source.csv", csvDoc1.get("filename").get(0).asText());
@@ -35,7 +56,7 @@ public class VerifyIngestResult {
     assertEquals("Carbonara", csvDoc1.get("my_name").get(0).asText());
     assertEquals(30, csvDoc1.get("my_price").get(0).asInt());
 
-    JsonNode csvDoc2 = docs.get(1);
+    JsonNode csvDoc2 = docs.get(4);
     assertEquals("source.csv-2", csvDoc2.get("id").asText());
     assertEquals("/conf/source.csv", csvDoc2.get("source").get(0).asText());
     assertEquals("source.csv", csvDoc2.get("filename").get(0).asText());
@@ -43,30 +64,12 @@ public class VerifyIngestResult {
     assertEquals("Pizza", csvDoc2.get("my_name").get(0).asText());
     assertEquals(10, csvDoc2.get("my_price").get(0).asInt());
 
-    JsonNode csvDoc3 = docs.get(2);
+    JsonNode csvDoc3 = docs.get(5);
     assertEquals("source.csv-3", csvDoc3.get("id").asText());
     assertEquals("/conf/source.csv", csvDoc3.get("source").get(0).asText());
     assertEquals("source.csv", csvDoc3.get("filename").get(0).asText());
     assertEquals("Korea", csvDoc3.get("my_country").get(0).asText());
     assertEquals("Tofu Soup", csvDoc3.get("my_name").get(0).asText());
     assertEquals(12, csvDoc3.get("my_price").get(0).asInt());
-
-    JsonNode jsonDoc1 = docs.get(3);
-    assertEquals("3", jsonDoc1.get("id").asText());
-    assertEquals("USA", jsonDoc1.get("my_country").get(0).asText());
-    assertEquals("Burger", jsonDoc1.get("my_name").get(0).asText());
-    assertEquals(30, jsonDoc1.get("my_price").get(0).asInt());
-
-    JsonNode jsonDoc2 = docs.get(4);
-    assertEquals("4", jsonDoc2.get("id").asText());
-    assertEquals("USA", jsonDoc2.get("my_country").get(0).asText());
-    assertEquals("Salad", jsonDoc2.get("my_name").get(0).asText());
-    assertEquals(10, jsonDoc2.get("my_price").get(0).asInt());
-
-    JsonNode jsonDoc3 = docs.get(5);
-    assertEquals("5", jsonDoc3.get("id").asText());
-    assertEquals("France", jsonDoc3.get("my_country").get(0).asText());
-    assertEquals("Wine", jsonDoc3.get("my_name").get(0).asText());
-    assertEquals(12, jsonDoc3.get("my_price").get(0).asInt());
   }
 }
