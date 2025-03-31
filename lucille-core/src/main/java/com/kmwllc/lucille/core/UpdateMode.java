0d337330e74fdbc547a7b93a67d155b59773889c
Jake Squatrito <jake.squatrito@icloud.com>
2025-03-31 12:12:01 -0400

    LC-237: Wrapped up Document JavaDoc

--- lucille-core/src/main/java/com/kmwllc/lucille/core/UpdateMode.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/core/UpdateMode.java
@@ -2,6 +2,11 @@ package com.kmwllc.lucille.core;
 
 import com.typesafe.config.Config;
 
+/**
+ * <p>APPEND: the provided values will be appended to the field.
+ * <p>OVERWRITE: the provided values will overwrite any current field values
+ * <p>SKIP: the provided values will populate the field if the field didn't previously exist; otherwise no change will be made.
+ */
 public enum UpdateMode {
   APPEND("append"), OVERWRITE("overwrite"), SKIP("skip");
 
