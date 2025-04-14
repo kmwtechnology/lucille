0482f170e497667cd7403823937efa7b7cf36e7c
Jake Squatrito <jake.squatrito@icloud.com>
2025-04-14 09:27:09 -0400

    LC-655: Testing connection to database. Script is executing correctly, etc.

--- lucille-core/src/test/java/com/kmwllc/lucille/connector/storageclient/StorageClientStateManagerTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/connector/storageclient/StorageClientStateManagerTest.java
@@ -1,10 +1,28 @@
 package com.kmwllc.lucille.connector.storageclient;
 
+import static org.junit.Assert.assertEquals;
+
 import com.kmwllc.lucille.connector.jdbc.DBTestHelper;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.junit.Rule;
+import org.junit.Test;
 
 public class StorageClientStateManagerTest {
 
   @Rule
-  public final DBTestHelper dbHelper = new DBTestHelper("org.h2.Driver", "jdbc:h2:mem:test", "", "", "db-test-start.sql", "db-test-end.sql");
+  public final DBTestHelper dbHelper = new DBTestHelper("org.h2.Driver", "jdbc:h2:mem:test", "", "",
+      "sm-db-test-start.sql", "sm-db-test-end.sql");
+
+  @Test
+  public void testStateManagerRootDirectory() throws Exception {
+    assertEquals(1, dbHelper.checkNumConnections());
+
+    Config config = ConfigFactory.parseResourcesAnySyntax("StorageClientStateManagerTest/config.conf");
+    StorageClientStateManager manager = new StorageClientStateManager(config);
+
+    manager.init();
+
+    manager.shutdown();
+  }
 }
