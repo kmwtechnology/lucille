525e29a6df5cbc7694e24278ff6a2117677bfada
Otto Cline <cline.o@northeastern.edu>
2026-02-24 18:00:54 -0500

    fixed tests to account for ignorefields defaulting to empty set instead of null

--- lucille-core/src/test/java/com/kmwllc/lucille/core/JsonDocumentTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/core/JsonDocumentTest.java
@@ -112,17 +112,6 @@ public class JsonDocumentTest extends DocumentTest.NodeDocumentTest {
   }
 
   @Test
-  public void testCreateFromJsonStringWithNullIgnoreFields() throws Exception {
-    String json = "{\"id\":\"123\", \"field1\":\"val1\", \"field2\":\"val2\"}";
-    Set<String> ignoreFields = null;
-
-    Document document = JsonDocument.fromJsonString(json, null, ignoreFields);
-    assertEquals("123", document.getId());
-    assertEquals("val1", document.getString("field1"));
-    assertEquals("val2", document.getString("field2"));
-  }
-
-  @Test
   public void testByteArraySerializationMultivalued() throws Exception {
     byte[] value1 = new byte[]{0x3c, 0x4c, 0x5c};
     byte[] value2 = new byte[]{0x4c, 0x4c, 0x5c};
