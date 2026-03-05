f2f260bd0441d02584f96d6d3fd5dd3dbb18bb96
Otto Cline <cline.o@northeastern.edu>
2026-03-05 13:15:08 -0500

    Javadoc annotating class purpose

--- lucille-core/src/main/java/com/kmwllc/lucille/util/FieldFilter.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/util/FieldFilter.java
@@ -3,11 +3,21 @@ package com.kmwllc.lucille.util;
 import com.typesafe.config.Config;
 import java.util.List;
 
+/**
+ * Standardizes whitelist and blacklist implementations for document fields. Applicable to any class which processes documents.
+ */
 public class FieldFilter {
 
   private final List<String> whitelist;
   private final List<String> blacklist;
 
+  /**
+   * If no naming schematic is passed in but the class still uses a whitelist or blacklist, default name to "whitelist" and
+   * "blacklist"
+   * @param config
+   * @param whitelistKey
+   * @param blacklistKey
+   */
   public FieldFilter(Config config, String whitelistKey, String blacklistKey) {
 
     String resolvedWhitelistKey = whitelistKey != null ? whitelistKey : "whitelist";
