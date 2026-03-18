ad437f83782c5e38db2c6f38d56f8d42af389bc2
Otto Cline <cline.o@northeastern.edu>
2026-03-18 15:52:32 -0400

    .this for constructor convention

--- lucille-core/src/main/java/com/kmwllc/lucille/util/FieldFilter.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/util/FieldFilter.java
@@ -23,8 +23,8 @@ public class FieldFilter {
     String resolvedWhitelistKey = whitelistKey != null ? whitelistKey : "whitelist";
     String resolvedBlacklistKey = blacklistKey != null ? blacklistKey : "blacklist";
 
-    whitelist = config.hasPath(resolvedWhitelistKey) ? config.getStringList(resolvedWhitelistKey) : List.of();
-    blacklist = config.hasPath(resolvedBlacklistKey) ? config.getStringList(resolvedBlacklistKey) : List.of();
+    this.whitelist = config.hasPath(resolvedWhitelistKey) ? config.getStringList(resolvedWhitelistKey) : List.of();
+    this.blacklist = config.hasPath(resolvedBlacklistKey) ? config.getStringList(resolvedBlacklistKey) : List.of();
   }
 
   public boolean isActive() {
