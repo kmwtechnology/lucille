f87c631ccd42b37cf336db77ba895736fb016957
Otto Cline <cline.o@northeastern.edu>
2026-03-24 13:35:59 -0400

    Test getters and nonspecified field

--- lucille-core/src/test/java/com/kmwllc/lucille/util/FieldFilterTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/util/FieldFilterTest.java
@@ -1,10 +1,13 @@
 package com.kmwllc.lucille.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import java.util.List;
 import org.junit.Test;
 
 public class FieldFilterTest {
@@ -25,6 +28,24 @@ public class FieldFilterTest {
     assertFalse(filter.shouldInclude("field1"));
     assertFalse(filter.shouldInclude("field2"));
     assertTrue(filter.shouldInclude("field3"));
+  }
+
+  @Test
+  public void testGetters() {
+    Config config = ConfigFactory.parseString(
+        "keepFields = [field1, field2, field3]\n" +
+            "ignoreFields = [field3]"
+    );
+
+    FieldFilter filter = new FieldFilter(config, "keepFields", "ignoreFields");
+    assertEquals(List.of("field1", "field2", "field3"), filter.getWhitelist());
+    assertEquals(List.of("field3"), filter.getBlacklist());
+
+    assertThrows(Exception.class, () -> filter.getWhitelist().add("field100"));
+    assertEquals(List.of("field1", "field2", "field3"), filter.getWhitelist());
+
+    assertThrows(Exception.class, () -> filter.getBlacklist().add("field200"));
+    assertEquals(List.of("field1", "field2", "field3"), filter.getWhitelist());
   }
 
   @Test
@@ -56,6 +77,9 @@ public class FieldFilterTest {
     assertTrue(filter.shouldInclude("field1"));
     assertTrue(filter.shouldInclude("field2"));
     assertFalse(filter.shouldInclude("field3"));
+
+    // field4 should not be included because it is not in the whitelist
+    assertFalse(filter.shouldInclude("field4"));
   }
 
   @Test
