4209d916055cb96339b8eddd74f5de5782944922
Otto Cline <cline.o@northeastern.edu>
2026-05-18 10:04:10 -0400

    Decided to go with skipDocument for the name

--- lucille-core/src/main/java/com/kmwllc/lucille/stage/SetStaticValues.java
+++ lucille-core/src/main/java/com/kmwllc/lucille/stage/SetStaticValues.java
@@ -20,7 +20,7 @@ import java.util.Map;
  * <ul>
  *   <li>staticValues (Map&lt;String, Object&gt;) : A mapping from the field to the value.</li>
  *   <li>updateMode (UpdateMode) : The update mode to use when updating the fields.</li>
- *   <li>skipDoc (boolean) : Sets the document to be skipped by the rest of the pipeline. Can be used in place of the {@link SkipDocument}
+ *   <li>skipDocument (boolean) : Sets the document to be skipped by the rest of the pipeline. Can be used in place of the {@link SkipDocument}
  *   stage when we want to both skip and delete documents, and don't want to add another stage with duplicate conditions.</li>
  * </ul>
  */
@@ -28,25 +28,25 @@ public class SetStaticValues extends Stage {
 
   public static final Spec SPEC = SpecBuilder.stage()
       .optionalString("updateMode")
-      .optionalBoolean("skipDoc")
+      .optionalBoolean("skipDocument")
       .requiredParent("staticValues", new TypeReference<Map<String, Object>>() {}).build();
 
   private final Map<String, Object> staticValues;
   private final UpdateMode updateMode;
-  private final boolean skipDoc;
+  private final boolean skipDocument;
 
   public SetStaticValues(Config config) {
     super(config);
 
     this.staticValues = config.getConfig("staticValues").root().unwrapped();
     this.updateMode = UpdateMode.fromConfig(config);
-    this.skipDoc = ConfigUtils.getOrDefault(config, "skipDoc", false);
+    this.skipDocument = ConfigUtils.getOrDefault(config, "skipDocument", false);
   }
 
   @Override
   public Iterator<Document> processDocument(Document doc) throws StageException {
     staticValues.forEach((fieldName, staticValue) -> doc.update(fieldName, updateMode, staticValue));
-    if (skipDoc) {
+    if (skipDocument) {
       doc.setSkipped(true);
     }
     return null;
