f353ee832bf2ecdb2b343a4573ca4c9b63eccd2a
mrwrench24 <jake.squatrito@icloud.com>
2026-06-18 15:55:11 -0400

    require dropwizard conf in entrypoint of api image

--- lucille-plugins/lucille-api/docker-entrypoint.sh
+++ lucille-plugins/lucille-api/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ -z "$DROPWIZARD_CONF"]; then
+  echo "ERROR: DROPWIZARD_CONF must be set to the path of your API config file."
+  echo "Example: docker run --env DROPWIZARD_CONF=/lucille/conf/api.yml ..."
+  exit 1
+fi
+
 exec java \
   ${JAVA_OPTS} \
   -Dconfig.file="${LUCILLE_CONF}" \
