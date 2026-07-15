f5f27732e02488a22b8f385c5d033fc42d001957
mrwrench24 <jake.squatrito@icloud.com>
2026-07-15 10:27:10 -0400

    missing import

--- lucille-core/src/test/java/com/kmwllc/lucille/connector/FileConnectorTest.java
+++ lucille-core/src/test/java/com/kmwllc/lucille/connector/FileConnectorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
